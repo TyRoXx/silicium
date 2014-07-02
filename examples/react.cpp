9931719b8673be64004c4c3eb0e39ed636e89452
TyRoXx <tyroxxxx@gmail.com>
2014-07-02 19:33:25 +0200

    move filter to new header

--- examples/react.cpp
+++ examples/react.cpp
@@ -6,6 +6,7 @@
 #include <reactive/transform.hpp>
 #include <reactive/while.hpp>
 #include <reactive/finite_state_machine.hpp>
+#include <reactive/filter.hpp>
 #include <SDL2/SDL.h>
 #include <boost/system/system_error.hpp>
 #include <boost/scope_exit.hpp>
@@ -14,63 +15,6 @@
 
 namespace rx
 {
-	template <class Input, class Predicate>
-	struct filter_observable
-			: public observable<typename Input::element_type>
-			, private observer<typename Input::element_type>
-	{
-		typedef typename Input::element_type element_type;
-
-		filter_observable(Input input, Predicate is_propagated)
-			: input(std::move(input))
-			, is_propagated(std::move(is_propagated))
-		{
-		}
-
-		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
-		{
-			assert(!receiver_);
-			receiver_ = &receiver;
-			input.async_get_one(*this);
-		}
-
-		virtual void cancel() SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-		}
-
-	private:
-
-		Input input;
-		Predicate is_propagated;
-		observer<element_type> *receiver_ = nullptr;
-
-		virtual void got_element(element_type value) SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-			if (!is_propagated(static_cast<typename std::add_const<element_type>::type>(value)))
-			{
-				input.async_get_one(*this);
-				return;
-			}
-			exchange(receiver_, nullptr)->got_element(std::move(value));
-		}
-
-		virtual void ended() SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-			exchange(receiver_, nullptr)->ended();
-		}
-	};
-
-	template <class Input, class Predicate>
-	auto filter(Input &&input, Predicate &&is_propagated)
-	{
-		return filter_observable<
-				typename std::decay<Input>::type,
-				typename std::decay<Predicate>::type>(std::forward<Input>(input), std::forward<Predicate>(is_propagated));
-	}
-
 	struct timer_elapsed
 	{
 	};
