e3358ede9d22220ed2ef1c5622ff84d9f364e2dc
TyRoXx <tyroxxxx@gmail.com>
2014-07-02 19:34:56 +0200

    add header for timer

--- examples/react.cpp
+++ examples/react.cpp
@@ -7,59 +7,12 @@
 #include <reactive/while.hpp>
 #include <reactive/finite_state_machine.hpp>
 #include <reactive/filter.hpp>
+#include <reactive/timer.hpp>
 #include <SDL2/SDL.h>
 #include <boost/system/system_error.hpp>
 #include <boost/scope_exit.hpp>
 #include <boost/variant.hpp>
 #include <boost/asio.hpp>
-
-namespace rx
-{
-	struct timer_elapsed
-	{
-	};
-
-	struct timer : observable<timer_elapsed>
-	{
-		typedef timer_elapsed element_type;
-
-		explicit timer(boost::asio::io_service &io, boost::posix_time::time_duration delay)
-			: impl(io)
-			, delay(delay)
-		{
-		}
-
-		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
-		{
-			assert(!receiver_);
-			receiver_ = &receiver;
-			impl.expires_from_now(delay);
-			impl.async_wait([this](boost::system::error_code error)
-			{
-				if (error)
-				{
-					assert(error == boost::asio::error::operation_aborted); //TODO: remove this assumption
-					assert(!receiver_); //cancel() should have reset the receiver already
-					return;
-				}
-				exchange(receiver_, nullptr)->got_element(timer_elapsed{});
-			});
-		}
-
-		virtual void cancel() SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-			impl.cancel();
-			receiver_ = nullptr;
-		}
-
-	private:
-
-		boost::asio::deadline_timer impl;
-		boost::posix_time::time_duration delay;
-		observer<element_type> *receiver_ = nullptr;
-	};
-}
 
 namespace
 {
---
+++ reactive/timer.hpp
@@ -0,0 +1,55 @@
+#ifndef SILICIUM_REACTIVE_TIMER_HPP
+#define SILICIUM_REACTIVE_TIMER_HPP
+
+#include <reactive/observable.hpp>
+#include <boost/asio/deadline_timer.hpp>
+
+namespace rx
+{
+	struct timer_elapsed
+	{
+	};
+
+	struct timer : observable<timer_elapsed>
+	{
+		typedef timer_elapsed element_type;
+
+		explicit timer(boost::asio::io_service &io, boost::posix_time::time_duration delay)
+			: impl(io)
+			, delay(delay)
+		{
+		}
+
+		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
+		{
+			assert(!receiver_);
+			receiver_ = &receiver;
+			impl.expires_from_now(delay);
+			impl.async_wait([this](boost::system::error_code error)
+			{
+				if (error)
+				{
+					assert(error == boost::asio::error::operation_aborted); //TODO: remove this assumption
+					assert(!receiver_); //cancel() should have reset the receiver already
+					return;
+				}
+				exchange(receiver_, nullptr)->got_element(timer_elapsed{});
+			});
+		}
+
+		virtual void cancel() SILICIUM_OVERRIDE
+		{
+			assert(receiver_);
+			impl.cancel();
+			receiver_ = nullptr;
+		}
+
+	private:
+
+		boost::asio::deadline_timer impl;
+		boost::posix_time::time_duration delay;
+		observer<element_type> *receiver_ = nullptr;
+	};
+}
+
+#endif
