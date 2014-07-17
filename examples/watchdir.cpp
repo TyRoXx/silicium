ec0ab51cd66663a0957c55bc044816823a489008
TyRoXx <tyroxxxx@gmail.com>
2014-07-17 21:07:07 +0200

    watch all the events; RAII for the watch_descriptor

--- examples/watchdir.cpp
+++ examples/watchdir.cpp
@@ -5,29 +5,54 @@
 #include <reactive/consume.hpp>
 #include <boost/asio.hpp>
 #include <sys/inotify.h>
+#include <dirent.h>
 
 namespace rx
 {
 	struct file_system_change
 	{
+		boost::uint32_t mask;
 		boost::filesystem::path name;
 	};
 
 	struct watch_descriptor
 	{
-		watch_descriptor()
+		watch_descriptor() BOOST_NOEXCEPT
 		{
 		}
 
-		watch_descriptor(int notifier, int watch)
+		watch_descriptor(int notifier, int watch) BOOST_NOEXCEPT
 			: notifier(notifier)
 			, watch(watch)
 		{
 		}
 
+		watch_descriptor(watch_descriptor &&other) BOOST_NOEXCEPT
+			: notifier(other.notifier)
+			, watch(other.watch)
+		{
+			other.notifier = -1;
+		}
+
+		~watch_descriptor() BOOST_NOEXCEPT
+		{
+			if (notifier == -1)
+			{
+				return;
+			}
+			inotify_rm_watch(notifier, watch);
+		}
+
+		watch_descriptor &operator = (watch_descriptor &&other) BOOST_NOEXCEPT
+		{
+			boost::swap(notifier, other.notifier);
+			boost::swap(watch, other.watch);
+			return *this;
+		}
+
 	private:
 
-		int notifier;
+		int notifier = -1;
 		int watch;
 	};
 
@@ -54,15 +79,14 @@ namespace rx
 			}
 		}
 
-		~file_system_notifier()
+		~file_system_notifier() BOOST_NOEXCEPT
 		{
 			close(notifier.native_handle());
 		}
 
-		watch_descriptor watch(boost::filesystem::path const &target)
+		watch_descriptor watch(boost::filesystem::path const &target, boost::uint32_t mask)
 		{
-			boost::uint32_t flags = IN_CREATE | IN_DELETE;
-			int wd = inotify_add_watch(notifier.native_handle(), target.string().c_str(), flags);
+			int wd = inotify_add_watch(notifier.native_handle(), target.string().c_str(), mask);
 			if (wd < 0)
 			{
 				throw boost::system::system_error(errno, boost::system::posix_category);
@@ -73,7 +97,9 @@ namespace rx
 		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
 		{
 			assert(!receiver_);
-			read_buffer.resize(8192);
+			std::size_t const min_buffer_size = sizeof(inotify_event) + NAME_MAX + 1;
+			std::size_t const additional_buffer = 8192;
+			read_buffer.resize(min_buffer_size + additional_buffer);
 			notifier.async_read_some(boost::asio::buffer(read_buffer), [this](boost::system::error_code error, std::size_t bytes_read)
 			{
 				if (error)
@@ -90,7 +116,7 @@ namespace rx
 					for (std::size_t i = 0; i < bytes_read; )
 					{
 						inotify_event const &event = *reinterpret_cast<inotify_event const *>(read_buffer.data() + i);
-						changes.emplace_back(file_system_change{event.name});
+						changes.emplace_back(file_system_change{event.mask, event.name});
 						i += sizeof(inotify_event);
 						i += event.len;
 					}
@@ -123,12 +149,12 @@ int main()
 {
 	boost::asio::io_service io;
 	rx::file_system_notifier notifier(io);
-	auto w = notifier.watch("/home/virtual/dev");
+	auto w = notifier.watch("/home/virtual/dev", IN_ALL_EVENTS);
 	auto printer = rx::transform(rx::ref(notifier), [](rx::file_system_notifier::element_type const &events) -> rx::detail::nothing
 	{
 		for (auto const &event : events)
 		{
-			std::cerr << event.name << '\n';
+			std::cerr << event.mask << " " << event.name << '\n';
 		}
 		return rx::detail::nothing{};
 	});
