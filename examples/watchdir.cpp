bd1230847bf87744a43607874af3744be25203ab
TyRoXx <tyroxxxx@gmail.com>
2014-07-17 21:50:38 +0200

    rename the inotify_observable and move it into namespace linux

--- examples/watchdir.cpp
+++ examples/watchdir.cpp
@@ -10,148 +10,147 @@
 
 namespace rx
 {
-	struct file_system_change
+	namespace linux
 	{
-		boost::uint32_t mask;
-		boost::filesystem::path name;
-	};
-
-	struct watch_descriptor
-	{
-		watch_descriptor() BOOST_NOEXCEPT
+		struct file_notification
 		{
-		}
+			boost::uint32_t mask;
+			boost::filesystem::path name;
+		};
 
-		watch_descriptor(int notifier, int watch) BOOST_NOEXCEPT
-			: notifier(notifier)
-			, watch(watch)
+		struct watch_descriptor
 		{
-		}
-
-		watch_descriptor(watch_descriptor &&other) BOOST_NOEXCEPT
-			: notifier(other.notifier)
-			, watch(other.watch)
-		{
-			other.notifier = -1;
-		}
-
-		~watch_descriptor() BOOST_NOEXCEPT
-		{
-			if (notifier == -1)
+			watch_descriptor() BOOST_NOEXCEPT
 			{
-				return;
 			}
-			inotify_rm_watch(notifier, watch);
-		}
 
-		watch_descriptor &operator = (watch_descriptor &&other) BOOST_NOEXCEPT
-		{
-			boost::swap(notifier, other.notifier);
-			boost::swap(watch, other.watch);
-			return *this;
-		}
-
-	private:
-
-		int notifier = -1;
-		int watch = -1;
-	};
-
-	struct file_system_notifier : observable<std::vector<file_system_change>>
-	{
-		typedef std::vector<file_system_change> element_type;
-
-		explicit file_system_notifier(boost::asio::io_service &io)
-			: notifier(io)
-		{
-			int fd = inotify_init();
-			if (fd < 0)
+			watch_descriptor(int notifier, int watch) BOOST_NOEXCEPT
+				: notifier(notifier)
+				, watch(watch)
 			{
-				throw boost::system::system_error(errno, boost::system::posix_category);
 			}
-			try
-			{
-				notifier.assign(fd);
-			}
-			catch (...)
-			{
-				close(fd);
-				throw;
-			}
-		}
 
-		~file_system_notifier() BOOST_NOEXCEPT
-		{
-			close(notifier.native_handle());
-		}
-
-		watch_descriptor watch(boost::filesystem::path const &target, boost::uint32_t mask)
-		{
-			int wd = inotify_add_watch(notifier.native_handle(), target.string().c_str(), mask);
-			if (wd < 0)
+			watch_descriptor(watch_descriptor &&other) BOOST_NOEXCEPT
+				: notifier(other.notifier)
+				, watch(other.watch)
 			{
-				throw boost::system::system_error(errno, boost::system::posix_category);
+				other.notifier = -1;
 			}
-			return watch_descriptor(notifier.native_handle(), wd);
-		}
 
-		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
-		{
-			assert(!receiver_);
-			std::size_t const min_buffer_size = sizeof(inotify_event) + NAME_MAX + 1;
-			std::size_t const additional_buffer = 8192;
-			read_buffer.resize(min_buffer_size + additional_buffer);
-			notifier.async_read_some(boost::asio::buffer(read_buffer), [this](boost::system::error_code error, std::size_t bytes_read)
+			~watch_descriptor() BOOST_NOEXCEPT
 			{
-				if (error)
+				if (notifier == -1)
 				{
-					if (error == boost::asio::error::operation_aborted)
-					{
-						return;
-					}
-					throw std::logic_error("not implemented");
+					return;
 				}
-				else
-				{
-					std::vector<file_system_change> changes;
-					for (std::size_t i = 0; i < bytes_read; )
-					{
-						inotify_event const &event = *reinterpret_cast<inotify_event const *>(read_buffer.data() + i);
-						changes.emplace_back(file_system_change{event.mask, event.name});
-						i += sizeof(inotify_event);
-						i += event.len;
-					}
-					exchange(this->receiver_, nullptr)->got_element(std::move(changes));
-				}
-			});
-			receiver_ = &receiver;
-		}
+				inotify_rm_watch(notifier, watch);
+			}
 
-		virtual void cancel() SILICIUM_OVERRIDE
+			watch_descriptor &operator = (watch_descriptor &&other) BOOST_NOEXCEPT
+			{
+				boost::swap(notifier, other.notifier);
+				boost::swap(watch, other.watch);
+				return *this;
+			}
+
+		private:
+
+			int notifier = -1;
+			int watch = -1;
+		};
+
+		struct inotify_observable : observable<std::vector<file_notification>>
 		{
-			assert(receiver_);
-			notifier.cancel();
-			receiver_ = nullptr;
-		}
+			typedef std::vector<file_notification> element_type;
 
-	private:
+			explicit inotify_observable(boost::asio::io_service &io)
+				: notifier(io)
+			{
+				int fd = inotify_init();
+				if (fd < 0)
+				{
+					throw boost::system::system_error(errno, boost::system::posix_category);
+				}
+				try
+				{
+					notifier.assign(fd);
+				}
+				catch (...)
+				{
+					close(fd);
+					throw;
+				}
+			}
 
-		boost::asio::posix::stream_descriptor notifier;
-		std::vector<char> read_buffer;
-		observer<element_type> *receiver_ = nullptr;
-	};
-}
+			~inotify_observable() BOOST_NOEXCEPT
+			{
+				close(notifier.native_handle());
+			}
 
-namespace
-{
+			watch_descriptor watch(boost::filesystem::path const &target, boost::uint32_t mask)
+			{
+				int wd = inotify_add_watch(notifier.native_handle(), target.string().c_str(), mask);
+				if (wd < 0)
+				{
+					throw boost::system::system_error(errno, boost::system::posix_category);
+				}
+				return watch_descriptor(notifier.native_handle(), wd);
+			}
+
+			virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
+			{
+				assert(!receiver_);
+				std::size_t const min_buffer_size = sizeof(inotify_event) + NAME_MAX + 1;
+				std::size_t const additional_buffer = 8192;
+				read_buffer.resize(min_buffer_size + additional_buffer);
+				notifier.async_read_some(boost::asio::buffer(read_buffer), [this](boost::system::error_code error, std::size_t bytes_read)
+				{
+					if (error)
+					{
+						if (error == boost::asio::error::operation_aborted)
+						{
+							return;
+						}
+						throw std::logic_error("not implemented");
+					}
+					else
+					{
+						std::vector<file_notification> changes;
+						for (std::size_t i = 0; i < bytes_read; )
+						{
+							inotify_event const &event = *reinterpret_cast<inotify_event const *>(read_buffer.data() + i);
+							changes.emplace_back(file_notification{event.mask, event.name});
+							i += sizeof(inotify_event);
+							i += event.len;
+						}
+						exchange(this->receiver_, nullptr)->got_element(std::move(changes));
+					}
+				});
+				receiver_ = &receiver;
+			}
+
+			virtual void cancel() SILICIUM_OVERRIDE
+			{
+				assert(receiver_);
+				notifier.cancel();
+				receiver_ = nullptr;
+			}
+
+		private:
+
+			boost::asio::posix::stream_descriptor notifier;
+			std::vector<char> read_buffer;
+			observer<element_type> *receiver_ = nullptr;
+		};
+	}
 }
 
 int main()
 {
 	boost::asio::io_service io;
-	rx::file_system_notifier notifier(io);
+	rx::linux::inotify_observable notifier(io);
 	auto w = notifier.watch("/home/virtual/dev", IN_ALL_EVENTS);
-	auto printer = rx::transform(rx::enumerate(rx::ref(notifier)), [](rx::file_system_change const &event) -> rx::detail::nothing
+	auto printer = rx::transform(rx::enumerate(rx::ref(notifier)), [](rx::linux::file_notification const &event) -> rx::detail::nothing
 	{
 		std::cerr << event.mask << " " << event.name << '\n';
 		return rx::detail::nothing{};
