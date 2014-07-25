2f9086d6c2b287f93dcb200e01ce42b60eb76d12
TyRoXx <tyroxxxx@gmail.com>
2014-07-26 00:57:40 +0200

    keep the client coroutine alive until they finish

--- examples/rxhttp.cpp
+++ examples/rxhttp.cpp
@@ -4,6 +4,7 @@
 #include <reactive/total_consumer.hpp>
 #include <boost/asio.hpp>
 #include <boost/ref.hpp>
+#include <boost/range/algorithm/find_if.hpp>
 
 namespace rx
 {
@@ -265,6 +266,116 @@ namespace rx
 			throw std::logic_error("should not be called");
 		}
 	};
+
+	template <class T, class ...Args>
+	auto make_unique(Args &&...args)
+	{
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+
+	using nothing = detail::nothing;
+
+	template <class NothingObservableObservable>
+	struct flattener
+			: public observable<nothing>
+			, private observer<typename NothingObservableObservable::element_type>
+	{
+		explicit flattener(NothingObservableObservable input)
+			: input(std::move(input))
+		{
+		}
+
+		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
+		{
+			assert(!receiver_);
+			receiver_ = &receiver;
+			fetch();
+		}
+
+		virtual void cancel() SILICIUM_OVERRIDE
+		{
+			throw std::logic_error("to do");
+		}
+
+	private:
+
+		typedef typename NothingObservableObservable::element_type nothing_observable;
+
+		struct child : observer<nothing>
+		{
+			flattener &parent;
+			nothing_observable observed;
+
+			explicit child(flattener &parent, nothing_observable observed)
+				: parent(parent)
+				, observed(observed)
+			{
+			}
+
+			void start()
+			{
+				observed.async_get_one(*this);
+			}
+
+			virtual void got_element(nothing) SILICIUM_OVERRIDE
+			{
+				return start();
+			}
+
+			virtual void ended() SILICIUM_OVERRIDE
+			{
+				return parent.remove_child(*this);
+			}
+		};
+
+		NothingObservableObservable input;
+		bool input_ended = false;
+		observer<nothing> *receiver_ = nullptr;
+		std::vector<std::unique_ptr<child>> children;
+
+		void fetch()
+		{
+			return input.async_get_one(*this);
+		}
+
+		void remove_child(child &removing)
+		{
+			auto const i = boost::range::find_if(children, [&removing](std::unique_ptr<child> const &element)
+			{
+				return element.get() == &removing;
+			});
+			children.erase(i);
+			if (input_ended &&
+			    children.empty())
+			{
+				exchange(receiver_, nullptr)->ended();
+			}
+		}
+
+		virtual void got_element(nothing_observable value) SILICIUM_OVERRIDE
+		{
+			children.emplace_back(make_unique<child>(*this, std::move(value)));
+			child &new_child = *children.back();
+			new_child.start();
+			return fetch();
+		}
+
+		virtual void ended() SILICIUM_OVERRIDE
+		{
+			assert(receiver_);
+			input_ended = true;
+			if (children.empty())
+			{
+				exchange(receiver_, nullptr)->ended();
+			}
+		}
+	};
+
+	template <class NothingObservableObservable>
+	auto flatten(NothingObservableObservable &&input)
+	{
+		return flattener<typename std::decay<NothingObservableObservable>::type>(std::forward<NothingObservableObservable>(input));
+	}
 }
 
 namespace
@@ -285,6 +396,7 @@ namespace
 				{
 					return;
 				}
+				//TODO: respond
 			}));
 			return client_handler;
 		}
@@ -301,7 +413,7 @@ int main()
 	boost::asio::io_service io;
 	boost::asio::ip::tcp::acceptor acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4(), 8080));
 	rx::tcp_acceptor clients(acceptor);
-	auto handling_clients = rx::make_coroutine<events>([&clients](rx::yield_context<events> &yield) -> void
+	auto handling_clients = rx::flatten(rx::make_coroutine<events>([&clients](rx::yield_context<events> &yield) -> void
 	{
 		for (;;)
 		{
@@ -317,7 +429,7 @@ int main()
 				yield(std::move(context));
 			}
 		}
-	});
+	}));
 	auto all = rx::make_total_consumer(rx::ref(handling_clients));
 	all.start();
 	io.run();
