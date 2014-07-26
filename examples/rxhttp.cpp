e55a339480f7f5c2fa2c3a671b339fb1393a0344
TyRoXx <tyroxxxx@gmail.com>
2014-07-27 01:09:05 +0200

    move sending_observable to a header

--- examples/rxhttp.cpp
+++ examples/rxhttp.cpp
@@ -1,3 +1,4 @@
+#include <reactive/sending_observable.hpp>
 #include <reactive/flatten.hpp>
 #include <reactive/socket_observable.hpp>
 #include <reactive/observable_source.hpp>
@@ -14,49 +15,6 @@
 #include <boost/format.hpp>
 #include <boost/interprocess/sync/null_mutex.hpp>
 #include <boost/thread/future.hpp>
-
-namespace rx
-{
-	struct sending_observable : observable<boost::system::error_code>
-	{
-		typedef boost::system::error_code element_type;
-		typedef boost::iterator_range<char const *> buffer_type;
-
-		explicit sending_observable(boost::asio::ip::tcp::socket &socket, buffer_type buffer)
-			: socket(&socket)
-			, buffer(buffer)
-		{
-		}
-
-		virtual void async_get_one(observer<boost::system::error_code> &receiver) SILICIUM_OVERRIDE
-		{
-			assert(!receiver_);
-			if (buffer.empty())
-			{
-				return receiver.ended();
-			}
-			receiver_ = &receiver;
-			boost::asio::async_write(*socket, boost::asio::buffer(buffer.begin(), buffer.size()), [this](boost::system::error_code error, std::size_t bytes_sent)
-			{
-				assert(buffer.size() == static_cast<ptrdiff_t>(bytes_sent));
-				buffer = buffer_type();
-				return exchange(receiver_, nullptr)->got_element(error);
-			});
-		}
-
-		virtual void cancel() SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-			throw std::logic_error("to do");
-		}
-
-	private:
-
-		boost::asio::ip::tcp::socket *socket = nullptr;
-		buffer_type buffer;
-		observer<boost::system::error_code> *receiver_ = nullptr;
-	};
-}
 
 namespace Si
 {
