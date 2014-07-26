a944d70deb593ee3bcbf3c77cd55cfe5ded3d61a
TyRoXx <tyroxxxx@gmail.com>
2014-07-27 00:46:10 +0200

    move observable_source to a header

--- examples/rxhttp.cpp
+++ examples/rxhttp.cpp
@@ -1,3 +1,4 @@
+#include <reactive/observable_source.hpp>
 #include <reactive/tcp_acceptor.hpp>
 #include <reactive/coroutine.hpp>
 #include <silicium/http/http.hpp>
@@ -14,71 +15,6 @@
 
 namespace rx
 {
-	template <class Observable, class YieldContext>
-	struct observable_source : Si::source<typename Observable::element_type>
-	{
-		typedef typename Observable::element_type element_type;
-
-		observable_source(Observable input, YieldContext &yield)
-			: input(std::move(input))
-			, yield(yield)
-		{
-		}
-
-		virtual boost::iterator_range<element_type const *> map_next(std::size_t size) SILICIUM_OVERRIDE
-		{
-			(void)size;
-			return {};
-		}
-
-		virtual element_type *copy_next(boost::iterator_range<element_type *> destination) SILICIUM_OVERRIDE
-		{
-			using boost::begin;
-			using boost::end;
-			auto i = begin(destination);
-			for (; i != end(destination); ++i)
-			{
-				auto element = yield.get_one(input);
-				if (!element)
-				{
-					break;
-				}
-				*i = std::move(*element);
-			}
-			return i;
-		}
-
-		virtual boost::uintmax_t minimum_size() SILICIUM_OVERRIDE
-		{
-			return 0;
-		}
-
-		virtual boost::optional<boost::uintmax_t> maximum_size() SILICIUM_OVERRIDE
-		{
-			return boost::none;
-		}
-
-		virtual std::size_t skip(std::size_t count) SILICIUM_OVERRIDE
-		{
-			size_t skipped = 0;
-			while ((skipped < count) && yield.get_one(input))
-			{
-			}
-			return skipped;
-		}
-
-	private:
-
-		Observable input;
-		YieldContext &yield;
-	};
-
-	template <class Observable, class YieldContext>
-	auto make_observable_source(Observable &&input, YieldContext &yield) -> observable_source<typename std::decay<Observable>::type, YieldContext>
-	{
-		return observable_source<typename std::decay<Observable>::type, YieldContext>(std::forward<Observable>(input), yield);
-	}
-
 	struct incoming_bytes
 	{
 		char const *begin = nullptr, *end = nullptr;
