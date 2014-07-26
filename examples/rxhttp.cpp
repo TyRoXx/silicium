c8139c2cfdce369224eea1220c795ee3bd118739
TyRoXx <tyroxxxx@gmail.com>
2014-07-26 22:13:06 +0200

    minimize the number of coroutine context switches for a ~6x improvement in requests per second

--- examples/rxhttp.cpp
+++ examples/rxhttp.cpp
@@ -145,7 +145,17 @@ namespace rx
 
 	struct incoming_bytes
 	{
-		char const *begin, *end;
+		char const *begin = nullptr, *end = nullptr;
+
+		incoming_bytes() BOOST_NOEXCEPT
+		{
+		}
+
+		incoming_bytes(char const *begin, char const *end) BOOST_NOEXCEPT
+			: begin(begin)
+			, end(end)
+		{
+		}
 	};
 
 	typedef Si::fast_variant<incoming_bytes, boost::system::error_code> received_from_socket;
@@ -438,6 +448,96 @@ namespace rx
 	};
 }
 
+namespace Si
+{
+	namespace detail
+	{
+		struct error_stripper : boost::static_visitor<boost::optional<rx::incoming_bytes>>
+		{
+			boost::optional<rx::incoming_bytes> operator()(rx::incoming_bytes bytes) const
+			{
+				return bytes;
+			}
+
+			boost::optional<rx::incoming_bytes> operator()(boost::system::error_code) const
+			{
+				return boost::none;
+			}
+		};
+
+		boost::optional<rx::incoming_bytes> strip_error(rx::received_from_socket received)
+		{
+			return Si::apply_visitor(error_stripper{}, received);
+		}
+	}
+
+	struct received_from_socket_source : Si::source<char>
+	{
+		explicit received_from_socket_source(Si::source<rx::received_from_socket> &original)
+			: original(&original)
+		{
+		}
+
+		virtual boost::iterator_range<char const *> map_next(std::size_t) SILICIUM_OVERRIDE
+		{
+			assert(original);
+			if (rest.begin == rest.end)
+			{
+				auto next = Si::get(*original);
+				if (!next)
+				{
+					return {};
+				}
+				auto bytes = detail::strip_error(*next);
+				if (!bytes)
+				{
+					return {};
+				}
+				rest = *bytes;
+			}
+			return boost::iterator_range<char const *>(rest.begin, rest.end);
+		}
+
+		virtual char *copy_next(boost::iterator_range<char *> destination) SILICIUM_OVERRIDE
+		{
+			auto mapped = map_next(destination.size());
+			char * const copied = std::copy_n(mapped.begin(), std::min(destination.size(), mapped.size()), destination.begin());
+			skip(std::distance(destination.begin(), copied));
+			return copied;
+		}
+
+		virtual boost::uintmax_t minimum_size() SILICIUM_OVERRIDE
+		{
+			return std::distance(rest.begin, rest.end);
+		}
+
+		virtual boost::optional<boost::uintmax_t> maximum_size() SILICIUM_OVERRIDE
+		{
+			return boost::none;
+		}
+
+		virtual std::size_t skip(std::size_t count) SILICIUM_OVERRIDE
+		{
+			std::size_t skipped = 0;
+			auto const rest_size = std::distance(rest.begin, rest.end);
+			auto const rest_skipped = std::min<ptrdiff_t>(count, rest_size);
+			rest.begin += rest_skipped;
+			count -= rest_skipped;
+			skipped += rest_skipped;
+			if (count > 0)
+			{
+				throw std::logic_error("to do");
+			}
+			return skipped;
+		}
+
+	private:
+
+		Si::source<rx::received_from_socket> *original = nullptr;
+		rx::incoming_bytes rest{};
+	};
+}
+
 namespace
 {
 	using events = rx::shared_observable<rx::detail::nothing>;
@@ -445,9 +545,10 @@ namespace
 	void serve_client(boost::asio::ip::tcp::socket &client, rx::yield_context<rx::detail::nothing> &yield, boost::uintmax_t visitor_number)
 	{
 		std::vector<char> received(4096);
-		rx::socket_receiver_observable receiving(client, boost::make_iterator_range(received.data(), received.data() + received.size()));
+		rx::socket_observable receiving(client, boost::make_iterator_range(received.data(), received.data() + received.size()));
 		auto receiver = rx::make_observable_source(rx::ref(receiving), yield);
-		boost::optional<Si::http::request_header> request = Si::http::parse_header(receiver);
+		Si::received_from_socket_source bytes_receiver(receiver);
+		boost::optional<Si::http::request_header> request = Si::http::parse_header(bytes_receiver);
 		if (!request)
 		{
 			return;
@@ -475,7 +576,7 @@ namespace
 		boost::system::error_code error;
 		client.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
 
-		while (Si::get(receiver))
+		while (Si::get(bytes_receiver))
 		{
 		}
 	}
