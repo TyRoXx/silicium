3a54bffe624d238cf6a264b78e3bc7f62f39149d
TyRoXx <tyroxxxx@gmail.com>
2014-07-27 17:31:08 +0200

    create a CMake find_package script (tested only on Ubuntu yet)

---
+++ cmake/FindSilicium.cmake
@@ -0,0 +1,19 @@
+# - Try to find libsilicium
+# Once done this will define
+#  LIBSILICIUM_FOUND
+#  LIBSILICIUM_INCLUDE_DIRS
+#  LIBSILICIUM_LIBRARIES
+
+find_path(LIBSILICIUM_INCLUDE_DIR "override.hpp" HINTS "/usr/local/include/silicium" "/usr/include/silicium")
+find_library(LIBSILICIUM_LIBRARY "libsilicium.a" HINTS "/usr/local/lib" "/usr/lib")
+
+set(LIBSILICIUM_LIBRARIES ${LIBSILICIUM_LIBRARY})
+set(LIBSILICIUM_INCLUDE_DIRS ${LIBSILICIUM_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBSILICIUM_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(LibSilicium  DEFAULT_MSG
+                                  LIBSILICIUM_LIBRARY LIBSILICIUM_INCLUDE_DIR)
+
+mark_as_advanced(LIBSILICIUM_INCLUDE_DIR LIBSILICIUM_LIBRARY )
---
+++ find_package_test/test.cpp
@@ -0,0 +1,7 @@
+#include <silicium/write_file.hpp>
+#include <reactive/observable.hpp>
+
+int main()
+{
+	auto write_file = Si::write_file;
+}
