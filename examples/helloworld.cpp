43f59d12eef1ab9c43142794727f26cf1625cf7d
TyRoXx <tyroxxxx@gmail.com>
2014-07-28 23:27:47 +0200

    make result_printer copyable

--- examples/helloworld.cpp
+++ examples/helloworld.cpp
@@ -62,23 +62,23 @@ namespace
 	struct result_printer : boost::static_visitor<>
 	{
 		explicit result_printer(std::ostream &out)
-			: m_out(out)
+			: m_out(&out)
 		{
 		}
 
 		void operator()(Si::build_success) const
 		{
-			m_out << "success";
+			*m_out << "success";
 		}
 
 		void operator()(Si::build_failure const &failure) const
 		{
-			m_out << "failure: " << failure.short_description;
+			*m_out << "failure: " << failure.short_description;
 		}
 
 	private:
 
-		std::ostream &m_out;
+		std::ostream *m_out = nullptr;
 	};
 }
 
