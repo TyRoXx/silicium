53ab34038bcbb30e6ef4db0d7e61b420e64eaaa2
TyRoXx <tyroxxxx@gmail.com>
2014-08-03 18:58:54 +0200

    move transform_if_initialized to a header

--- examples/watchdir.cpp
+++ examples/watchdir.cpp
@@ -1,6 +1,7 @@
 #include <reactive/enumerate.hpp>
 #include <reactive/for_each.hpp>
 #include <reactive/ref.hpp>
+#include <reactive/transform_if_initialized.hpp>
 #ifdef _WIN32
 #	include <reactive/win32/directory_changes.hpp>
 #else
@@ -63,94 +64,6 @@ namespace rx
 			}
 			return rx::file_notification(*type, std::move(original.name));
 		}
-	}
-
-	template <class Element, class Input, class Transformation>
-	struct conditional_transformer
-		: public observable<Element>
-		, private observer<typename Input::element_type>
-	{
-		typedef Element element_type;
-
-		conditional_transformer()
-		{
-		}
-
-		explicit conditional_transformer(Input original, Transformation transform)
-			: original(std::move(original))
-			, transform(std::move(transform))
-		{
-		}
-
-#ifdef _MSC_VER
-		conditional_transformer(conditional_transformer &&other)
-			: original(std::move(other.original))
-			, transform(std::move(other.transform))
-			, receiver_(std::move(other.receiver_))
-		{
-		}
-
-		conditional_transformer &operator = (conditional_transformer &&other)
-		{
-			original = std:move(other.original);
-			transform = std:move(other.transform);
-			receiver_ = std:move(other.receiver_);
-			return *this;
-		}
-#endif
-
-		virtual void async_get_one(observer<element_type> &receiver) SILICIUM_OVERRIDE
-		{
-			assert(!receiver_);
-			receiver_ = &receiver;
-			fetch();
-		}
-
-		virtual void cancel() SILICIUM_OVERRIDE
-		{
-			throw std::logic_error("to do");
-		}
-
-	private:
-
-		Input original;
-		Transformation transform; //TODO: optimize for emptiness
-		observer<element_type> *receiver_ = nullptr;
-
-		BOOST_DELETED_FUNCTION(conditional_transformer(conditional_transformer const &));
-		BOOST_DELETED_FUNCTION(conditional_transformer &operator = (conditional_transformer const &));
-
-		void fetch()
-		{
-			original.async_get_one(*this);
-		}
-
-		virtual void got_element(typename Input::element_type value) SILICIUM_OVERRIDE
-		{
-			assert(receiver_);
-			auto converted = transform(std::move(value));
-			if (converted)
-			{
-				rx::exchange(receiver_, nullptr)->got_element(std::move(*converted));
-			}
-			else
-			{
-				fetch();
-			}
-		}
-
-		virtual void ended() SILICIUM_OVERRIDE
-		{
-			SILICIUM_UNREACHABLE();
-		}
-	};
-
-	template <class Element, class Input, class Transformation>
-	auto transform_if_initialized(Input &&input, Transformation &&transform) -> conditional_transformer<Element, typename std::decay<Input>::type, typename std::decay<Transformation>::type>
-	{
-		return conditional_transformer<Element, typename std::decay<Input>::type, typename std::decay<Transformation>::type>(
-			std::forward<Input>(input),
-			std::forward<Transformation>(transform));
 	}
 
 	struct file_system_watcher : observable<file_notification>
