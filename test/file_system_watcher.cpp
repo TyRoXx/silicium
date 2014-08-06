995a7c2e184cc0fd2dfc921d378fec0ab01c4aa9
TyRoXx <tyroxxxx@gmail.com>
2014-08-06 21:57:07 +0200

    test remove and change events with file_system_watcher

--- test/file_system_watcher.cpp
+++ test/file_system_watcher.cpp
@@ -13,7 +13,38 @@ namespace rx
 		void touch(boost::filesystem::path const &name)
 		{
 			std::ofstream file(name.string());
-			file.close();
+			BOOST_REQUIRE(file);
+		}
+
+		void test_single_event(
+			boost::filesystem::path const &watched_dir,
+			std::function<void ()> const &provoke_event,
+			std::function<void (file_notification const &)> const &on_event)
+		{
+			boost::asio::io_service io;
+			file_system_watcher watcher(io, watched_dir);
+
+			bool got_event = false;
+			auto consumer = consume<file_notification>([&io, &got_event, &on_event](file_notification const &event)
+			{
+				io.stop();
+				on_event(event);
+				got_event = true;
+			});
+			watcher.async_get_one(consumer);
+
+			provoke_event();
+
+			boost::asio::steady_timer timeout(io);
+			timeout.expires_from_now(std::chrono::seconds(1));
+			timeout.async_wait([&io](boost::system::error_code)
+			{
+				io.stop();
+			});
+
+			io.run();
+
+			BOOST_CHECK(got_event);
 		}
 	}
 
@@ -26,30 +57,56 @@ namespace rx
 			boost::filesystem::remove(test_file, ec);
 		}
 
-		boost::asio::io_service io;
-		file_system_watcher watcher(io, watched_dir);
-
-		bool got_event = false;
-		auto consumer = consume<file_notification>([&test_file, &io, &got_event](file_notification const &event)
+		test_single_event(
+			watched_dir,
+			std::bind(touch, test_file),
+			[&test_file](file_notification const &event)
 		{
-			io.stop();
+
 			BOOST_CHECK(file_notification_type::add == event.type);
 			BOOST_CHECK(equivalent(test_file, event.name));
-			got_event = true;
 		});
-		watcher.async_get_one(consumer);
+	}
+
+	BOOST_AUTO_TEST_CASE(file_system_watcher_remove)
+	{
+		boost::filesystem::path const watched_dir = boost::filesystem::current_path();
+		boost::filesystem::path const test_file = watched_dir / "test.txt";
 
 		touch(test_file);
 
-		boost::asio::steady_timer timeout(io);
-		timeout.expires_from_now(std::chrono::seconds(1));
-		timeout.async_wait([&io](boost::system::error_code)
+		test_single_event(
+			watched_dir,
+			[&test_file]
 		{
-			io.stop();
+			boost::filesystem::remove(test_file);
+		},
+			[&test_file, &watched_dir](file_notification const &event)
+		{
+			BOOST_CHECK(file_notification_type::remove == event.type);
+			BOOST_CHECK_EQUAL(test_file, watched_dir / event.name);
 		});
+	}
 
-		io.run();
+	BOOST_AUTO_TEST_CASE(file_system_watcher_change)
+	{
+		boost::filesystem::path const watched_dir = boost::filesystem::current_path();
+		boost::filesystem::path const test_file = watched_dir / "test.txt";
 
-		BOOST_CHECK(got_event);
+		touch(test_file);
+
+		test_single_event(
+			watched_dir,
+			[&test_file]
+		{
+			std::ofstream file(test_file.string());
+			BOOST_REQUIRE(file);
+			file << "hello\n";
+		},
+			[&test_file, &watched_dir](file_notification const &event)
+		{
+			BOOST_CHECK(file_notification_type::change == event.type);
+			BOOST_CHECK_EQUAL(test_file, watched_dir / event.name);
+		});
 	}
 }
