4f092eba1960710c616373d7b63d1f6adbe7bb60
TyRoXx <tyroxxxx@gmail.com>
2014-08-07 21:36:53 +0200

    CMake find scripts: Remove unnecessary LIB prefix from found variables

--- cmake/FindReactive.cmake
+++ cmake/FindReactive.cmake
@@ -1,19 +1,19 @@
 # - Try to find libREACTIVE
 # Once done this will define
-#  LIBREACTIVE_FOUND
-#  LIBREACTIVE_INCLUDE_DIRS
-#  LIBREACTIVE_LIBRARIES
+#  REACTIVE_FOUND
+#  REACTIVE_INCLUDE_DIRS
+#  REACTIVE_LIBRARIES
 
-find_path(LIBREACTIVE_INCLUDE_DIR "reactive/config.hpp" HINTS "/usr/local/include" "/usr/include")
-find_library(LIBREACTIVE_LIBRARY "libreactive.a" HINTS "/usr/local/lib" "/usr/lib")
+find_path(REACTIVE_INCLUDE_DIR "reactive/config.hpp" HINTS "/usr/local/include" "/usr/include")
+find_library(REACTIVE_LIBRARY "libreactive.a" HINTS "/usr/local/lib" "/usr/lib")
 
-set(LIBREACTIVE_LIBRARIES ${LIBREACTIVE_LIBRARY})
-set(LIBREACTIVE_INCLUDE_DIRS ${LIBREACTIVE_INCLUDE_DIR} ${LIBREACTIVE_INCLUDE_DIR})
+set(REACTIVE_LIBRARIES ${REACTIVE_LIBRARY})
+set(REACTIVE_INCLUDE_DIRS ${REACTIVE_INCLUDE_DIR} ${REACTIVE_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBREACTIVE_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(LibREACTIVE  DEFAULT_MSG
-                                  LIBREACTIVE_LIBRARY LIBREACTIVE_INCLUDE_DIR)
+                                  REACTIVE_LIBRARY REACTIVE_INCLUDE_DIR)
 
-mark_as_advanced(LIBREACTIVE_INCLUDE_DIR LIBREACTIVE_LIBRARY)
+mark_as_advanced(REACTIVE_INCLUDE_DIR REACTIVE_LIBRARY)
--- cmake/FindSilicium.cmake
+++ cmake/FindSilicium.cmake
@@ -1,19 +1,19 @@
 # - Try to find libsilicium
 # Once done this will define
-#  LIBSILICIUM_FOUND
-#  LIBSILICIUM_INCLUDE_DIRS
-#  LIBSILICIUM_LIBRARIES
+#  SILICIUM_FOUND
+#  SILICIUM_INCLUDE_DIRS
+#  SILICIUM_LIBRARIES
 
-find_path(LIBSILICIUM_INCLUDE_DIR "silicium/version.hpp" HINTS "/usr/local/include" "/usr/include")
-find_library(LIBSILICIUM_LIBRARY "libsilicium.a" HINTS "/usr/local/lib" "/usr/lib")
+find_path(SILICIUM_INCLUDE_DIR "silicium/version.hpp" HINTS "/usr/local/include" "/usr/include")
+find_library(SILICIUM_LIBRARY "libsilicium.a" HINTS "/usr/local/lib" "/usr/lib")
 
-set(LIBSILICIUM_LIBRARIES ${LIBSILICIUM_LIBRARY})
-set(LIBSILICIUM_INCLUDE_DIRS ${LIBSILICIUM_INCLUDE_DIR} ${LIBREACTIVE_INCLUDE_DIR})
+set(SILICIUM_LIBRARIES ${SILICIUM_LIBRARY})
+set(SILICIUM_INCLUDE_DIRS ${SILICIUM_INCLUDE_DIR} ${REACTIVE_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBSILICIUM_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(LibSilicium  DEFAULT_MSG
-                                  LIBSILICIUM_LIBRARY LIBSILICIUM_INCLUDE_DIR)
+                                  SILICIUM_LIBRARY SILICIUM_INCLUDE_DIR)
 
-mark_as_advanced(LIBSILICIUM_INCLUDE_DIR LIBSILICIUM_LIBRARY)
+mark_as_advanced(SILICIUM_INCLUDE_DIR SILICIUM_LIBRARY)
