500357f0ee3a3d58372432439f4e393d54139bf5
TyRoXx <tyroxxxx@gmail.com>
2014-08-08 21:52:38 +0200

    remove an unused function

--- examples/watchdir.cpp
+++ examples/watchdir.cpp
@@ -1,46 +1,7 @@
 #include <reactive/directory_watcher.hpp>
 #include <reactive/for_each.hpp>
-#include <boost/range/adaptor/filtered.hpp>
-#include <boost/range/numeric.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <iostream>
-
-namespace
-{
-#ifndef _WIN32
-	std::string inotify_mask_to_string(boost::uint32_t mask)
-	{
-		using flag_entry = std::pair<boost::uint32_t, std::string>;
-		static std::array<flag_entry, 12> const flag_names
-		{{
-			{IN_ACCESS, "access"},
-			{IN_MODIFY, "modify"},
-			{IN_ATTRIB, "attrib"},
-			{IN_CLOSE_WRITE, "close_write"},
-			{IN_CLOSE_NOWRITE, "close_nowrite"},
-			{IN_OPEN, "open"},
-			{IN_MOVED_FROM, "moved_from"},
-			{IN_MOVED_TO, "moved_to"},
-			{IN_CREATE, "create"},
-			{IN_DELETE, "delete"},
-			{IN_DELETE_SELF, "delete_self"},
-			{IN_MOVE_SELF, "move_self"}
-		}};
-		return boost::accumulate(flag_names | boost::adaptors::filtered([mask](flag_entry const &entry)
-		{
-			return (entry.first & mask) == entry.first;
-		}), std::string(), [](std::string left, flag_entry const &flag)
-		{
-			if (!left.empty())
-			{
-				left += "|";
-			}
-			left += flag.second;
-			return left;
-		});
-	}
-#endif
-}
 
 int main()
 {
