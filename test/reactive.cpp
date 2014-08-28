54351c08d910820701fc765345cf1c5a86dcb211
TyRoXx <tyroxxxx@gmail.com>
2014-08-28 23:27:13 +0200

    test get_one in a thread which is not implemented

--- test/reactive.cpp
+++ test/reactive.cpp
@@ -437,3 +437,41 @@ BOOST_AUTO_TEST_CASE(reactive_async)
 	a.wait();
 	BOOST_CHECK(expected == produced);
 }
+
+BOOST_AUTO_TEST_CASE(reactive_async_get_one)
+{
+	auto a = Si::make_thread<int, Si::std_threading>([](Si::yield_context<int> &yield)
+	{
+		auto b = Si::make_thread<int, Si::boost_threading>([](Si::yield_context<int> &yield)
+		{
+			yield(1);
+			yield(2);
+			yield(3);
+		});
+		for (;;)
+		{
+			auto e = yield.get_one(b);
+			if (!e)
+			{
+				break;
+			}
+			yield(std::move(*e));
+		}
+		b.wait();
+	});
+	std::vector<int> const expected{1, 2, 3};
+	std::vector<int> produced;
+	std::unique_ptr<Si::observer<int>> pusher;
+	pusher = Si::to_unique(Si::consume<int>([&](int element)
+	{
+		produced.emplace_back(element);
+		if (produced.size() == expected.size())
+		{
+			return;
+		}
+		a.async_get_one(*pusher);
+	}));
+	a.async_get_one(*pusher);
+	a.wait();
+	BOOST_CHECK(expected == produced);
+}
