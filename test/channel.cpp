2592f7cccfbfadd3edfb7835a6dc1142c00d72c1
TyRoXx <tyroxxxx@gmail.com>
2014-08-31 15:35:44 +0200

    channel works across thread with the help of a mutex

--- test/channel.cpp
+++ test/channel.cpp
@@ -1,5 +1,6 @@
 #include <silicium/coroutine.hpp>
 #include <silicium/consume.hpp>
+#include <silicium/thread.hpp>
 #include <boost/test/unit_test.hpp>
 
 namespace Si
@@ -13,6 +14,8 @@ namespace Si
 		template <class Message>
 		struct channel_common_state
 		{
+			//TODO: use a much simpler spinlock
+			boost::mutex access;
 			observer<Message> *receiver = nullptr;
 			observer<delivered> *delivery = nullptr;
 			Message *message = nullptr;
@@ -30,11 +33,15 @@ namespace Si
 
 			void async_get_one(observer<element_type> &receiver)
 			{
+				boost::unique_lock<boost::mutex> lock(state->access);
 				assert(!state->receiver);
 				if (state->message)
 				{
-					receiver.got_element(std::move(*Si::exchange(state->message, nullptr)));
-					Si::exchange(state->delivery, nullptr)->got_element(delivered());
+					auto * const message = Si::exchange(state->message, nullptr);
+					auto * const delivery = Si::exchange(state->delivery, nullptr);
+					lock.unlock();
+					receiver.got_element(std::move(*message));
+					delivery->got_element(delivered());
 				}
 				else
 				{
@@ -63,10 +70,13 @@ namespace Si
 
 			void set_message(Message &message)
 			{
+				boost::unique_lock<boost::mutex> lock(state->access);
 				assert(!state->message);
 				if (state->receiver)
 				{
-					Si::exchange(state->receiver, nullptr)->got_element(std::move(message));
+					auto * const receiver = Si::exchange(state->receiver, nullptr);
+					lock.unlock();
+					receiver->got_element(std::move(message));
 				}
 				else
 				{
@@ -76,6 +86,7 @@ namespace Si
 
 			void async_get_one(observer<element_type> &receiver)
 			{
+				boost::unique_lock<boost::mutex> lock(state->access);
 				assert(!state->delivery);
 				if (state->message)
 				{
@@ -84,6 +95,7 @@ namespace Si
 				else
 				{
 					assert(!state->receiver);
+					lock.unlock();
 					receiver.got_element(delivered());
 				}
 			}
@@ -138,7 +150,7 @@ namespace Si
 	}
 }
 
-BOOST_AUTO_TEST_CASE(channel_)
+BOOST_AUTO_TEST_CASE(channel_with_coroutine)
 {
 	Si::channel<int> channel;
 	auto t = Si::make_coroutine<int>([&channel](Si::yield_context<int> &yield)
@@ -164,5 +176,36 @@ BOOST_AUTO_TEST_CASE(channel_)
 	});
 	t.async_get_one(consumer);
 	s.async_get_one(consumer);
+	BOOST_CHECK(got);
+}
+
+BOOST_AUTO_TEST_CASE(channel_with_thread)
+{
+	Si::channel<int> channel;
+	auto t = Si::make_thread<int, Si::boost_threading>([&channel](Si::yield_context<int> &yield)
+	{
+		Si::send(channel, 2, yield);
+		Si::send(channel, 3, yield);
+	});
+	auto s = Si::make_thread<int, Si::boost_threading>([&channel](Si::yield_context<int> &yield)
+	{
+		auto a = Si::receive(channel, yield);
+		BOOST_REQUIRE(a);
+		auto b = Si::receive(channel, yield);
+		BOOST_REQUIRE(b);
+		int result = *a + *b;
+		yield(result);
+	});
+	bool got = false;
+	auto consumer = Si::consume<int>([&s, &got](int result)
+	{
+		BOOST_CHECK(!got);
+		got = true;
+		BOOST_CHECK_EQUAL(5, result);
+	});
+	t.async_get_one(consumer);
+	s.async_get_one(consumer);
+	t.wait();
+	s.wait();
 	BOOST_CHECK(got);
 }
