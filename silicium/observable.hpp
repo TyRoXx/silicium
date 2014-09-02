f09403fade53d839d37ddc0f07a4f169c49ad8a0
TyRoXx <tyroxxxx@gmail.com>
2014-09-02 15:57:48 +0200

    create a Boost concept for Observable

--- silicium/observable.hpp
+++ silicium/observable.hpp
@@ -2,6 +2,8 @@
 #define SILICIUM_REACTIVE_OBSERVABLE_HPP
 
 #include <silicium/observer.hpp>
+#include <silicium/override.hpp>
+#include <boost/concept_check.hpp>
 
 namespace Si
 {
@@ -16,6 +18,50 @@ namespace Si
 
 		virtual void async_get_one(observer<element_type> &receiver) = 0;
 	};
+
+	template <class X>
+	struct Observable
+	{
+		using element_type = typename X::element_type;
+
+		BOOST_CONCEPT_USAGE(Observable)
+		{
+			X moved = std::move(observable);
+			moved = std::move(observable);
+			observable.async_get_one(observer);
+		}
+
+	private:
+
+		struct test_observer : observer<element_type>
+		{
+			virtual void got_element(element_type) SILICIUM_OVERRIDE
+			{
+			}
+
+			virtual void ended() SILICIUM_OVERRIDE
+			{
+			}
+		};
+
+		X observable;
+		test_observer observer;
+		element_type element;
+	};
+
+	namespace detail
+	{
+		//example and test for the smallest possible Observable
+		struct minimum_observable
+		{
+			using element_type = int;
+			void async_get_one(observer<element_type> &)
+			{
+			}
+		};
+
+		BOOST_CONCEPT_ASSERT((Observable<minimum_observable>));
+	}
 }
 
 #endif
