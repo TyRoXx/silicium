f5b2720ed350392956951ca1406ef8c6e0d11e67
TyRoXx <tyroxxxx@gmail.com>
2014-09-07 19:53:46 +0200

    add make_source_observable

--- test/brainfuck.cpp
+++ test/brainfuck.cpp
@@ -219,50 +219,6 @@ namespace bf
 	}
 }
 
-namespace Si
-{
-	template <class Element, class Source>
-	struct source_observable
-	{
-		using element_type = Element;
-
-		source_observable()
-		{
-		}
-
-		explicit source_observable(Source source)
-			: source(std::move(source))
-		{
-		}
-
-		void async_get_one(observer<Element> &receiver)
-		{
-			boost::optional<Element> value = Si::get(source);
-			if (value)
-			{
-				receiver.got_element(std::move(*value));
-			}
-			else
-			{
-				receiver.ended();
-			}
-		}
-
-	private:
-
-		Source source;
-	};
-
-	template <class Element, class Source>
-	auto make_source_observable(Source &&source)
-#if !SILICIUM_COMPILER_HAS_AUTO_RETURN_TYPE
-		-> source_observable<Element, typename std::decay<Source>::type>
-#endif
-	{
-		return source_observable<Element, typename std::decay<Source>::type>(std::forward<Source>(source));
-	}
-}
-
 BOOST_AUTO_TEST_CASE(bf_empty)
 {
 	Si::empty<char> input;
---
+++ test/source_observable.cpp
@@ -0,0 +1,54 @@
+#include <silicium/source_observable.hpp>
+#include <silicium/source.hpp>
+#include <silicium/consume.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace
+{
+	template <class Element>
+	struct end_observer : Si::observer<Element>
+	{
+		bool has_ended = false;
+
+		virtual void got_element(element_type value) SILICIUM_OVERRIDE
+		{
+			boost::ignore_unused_variable_warning(value);
+			BOOST_FAIL("no element expected");
+		}
+
+		virtual void ended() SILICIUM_OVERRIDE
+		{
+			BOOST_REQUIRE(!has_ended);
+			has_ended = true;
+		}
+	};
+}
+
+BOOST_AUTO_TEST_CASE(source_observable_empty)
+{
+	std::vector<char> const empty;
+	auto observable = Si::make_source_observable<char>(Si::make_container_source(empty));
+	end_observer<char> observer;
+	observable.async_get_one(observer);
+	BOOST_CHECK(observer.has_ended);
+}
+
+BOOST_AUTO_TEST_CASE(source_observable_non_empty)
+{
+	std::string const input = "A";
+	auto observable = Si::make_source_observable<char>(Si::make_container_source(input));
+	{
+		bool got_element = false;
+		auto observer = Si::consume<char>([&got_element](char c)
+		{
+			BOOST_CHECK_EQUAL('A', c);
+			BOOST_REQUIRE(!got_element);
+			got_element = true;
+		});
+		observable.async_get_one(observer);
+		BOOST_REQUIRE(got_element);
+	}
+	end_observer<char> observer;
+	observable.async_get_one(observer);
+	BOOST_CHECK(observer.has_ended);
+}
