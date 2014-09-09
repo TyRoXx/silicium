1ac1c669acf1d5d694a4e6dac03da5465762aed8
TyRoXx <tyroxxxx@gmail.com>
2014-09-09 18:31:30 +0200

    simplify proper_value_function using std::conditional

--- silicium/detail/proper_value_function.hpp
+++ silicium/detail/proper_value_function.hpp
@@ -7,20 +7,12 @@ namespace Si
 {
 	namespace detail
 	{
-		template <class F, bool IsProperValue, class R, class ...Args>
-		struct proper_value_function_impl
-		{
-			using type = std::function<R (Args...)>;
-		};
-
 		template <class F, class R, class ...Args>
-		struct proper_value_function_impl<F, true, R, Args...>
-		{
-			using type = F;
-		};
-
-		template <class F, class R, class ...Args>
-		using proper_value_function = proper_value_function_impl<F, std::is_default_constructible<F>::value && std::is_move_assignable<F>::value, R, Args...>;
+		using proper_value_function = std::conditional<
+			std::is_default_constructible<F>::value && std::is_move_assignable<F>::value,
+			F,
+			std::function<R (Args...)>
+		>;
 	}
 }
 
