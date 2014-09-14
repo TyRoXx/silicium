e0de7d9b3feb4497e421d882bb118f113fb856a7
TyRoXx <tyroxxxx@gmail.com>
2014-09-14 19:29:17 +0200

    create make_single_source

---
+++ silicium/single_source.hpp
@@ -0,0 +1,57 @@
+#ifndef SILICIUM_SINGLE_SOURCE_HPP
+#define SILICIUM_SINGLE_SOURCE_HPP
+
+#include <boost/range/iterator_range.hpp>
+
+namespace Si
+{
+	template <class Element>
+	struct single_source
+	{
+		using element_type = Element;
+
+		single_source()
+		{
+		}
+
+		explicit single_source(Element element)
+			: element(std::move(element))
+		{
+		}
+
+		boost::iterator_range<element_type const *> map_next(std::size_t size)
+		{
+			boost::ignore_unused_variable_warning(size);
+			if (used)
+			{
+				return {};
+			}
+			used = true;
+			return boost::make_iterator_range(&element, &element + 1);
+		}
+
+		element_type *copy_next(boost::iterator_range<element_type *> destination)
+		{
+			if (used || destination.empty())
+			{
+				return destination.begin();
+			}
+			*destination.begin() = std::move(element);
+			used = true;
+			return destination.begin() + 1;
+		}
+
+	private:
+
+		Element element;
+		bool used = false;
+	};
+
+	template <class Element>
+	auto make_single_source(Element &&element)
+	{
+		return single_source<typename std::decay<Element>::type>(std::forward<Element>(element));
+	}
+}
+
+#endif
