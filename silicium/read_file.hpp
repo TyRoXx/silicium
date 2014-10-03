6dc72ca3bc7c043feb6b24972023e8b6316598b6
TyRoXx <tyroxxxx@gmail.com>
2014-10-03 19:38:02 +0200

    remove read_file.cpp

--- silicium/read_file.hpp
+++ silicium/read_file.hpp
@@ -2,11 +2,16 @@
 #define SILICIUM_READ_FILE_HPP
 
 #include <boost/filesystem/path.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
 #include <vector>
 
 namespace Si
 {
-	std::vector<char> read_file(boost::filesystem::path const &file);
+	inline std::vector<char> read_file(boost::filesystem::path const &file)
+	{
+		boost::iostreams::mapped_file_source mapped(file);
+		return std::vector<char>(mapped.data(), mapped.data() + mapped.size());
+	}
 }
 
 #endif
