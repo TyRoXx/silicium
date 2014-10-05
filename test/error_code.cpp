2a9cc043a49f4dd9fb39a61f8b41ac352940278e
TyRoXx <tyroxxxx@gmail.com>
2014-10-05 16:37:15 +0200

    add more error_code tests

--- test/error_code.cpp
+++ test/error_code.cpp
@@ -3,6 +3,31 @@
 #include <boost/unordered_set.hpp>
 #include <unordered_set>
 
+BOOST_AUTO_TEST_CASE(error_code_default_constructor)
+{
+	Si::error_code<> const e;
+	BOOST_CHECK_EQUAL(boost::system::error_code(), e.to_underlying());
+	BOOST_CHECK_EQUAL(e, e);
+	BOOST_CHECK_EQUAL(0, e.value());
+	BOOST_CHECK_EQUAL(&boost::system::error_code().category(), &e.category());
+}
+
+BOOST_AUTO_TEST_CASE(error_code_create)
+{
+	Si::error_code<> const e = Si::error_code<>::create<1, boost::system::system_category>();
+	BOOST_CHECK_EQUAL(boost::system::error_code(1, boost::system::system_category()), e.to_underlying());
+	BOOST_CHECK_EQUAL(e, e);
+	BOOST_CHECK_EQUAL(1, e.value());
+	BOOST_CHECK_EQUAL(&boost::system::system_category(), &e.category());
+}
+
+BOOST_AUTO_TEST_CASE(error_code_clear)
+{
+	Si::error_code<> e = Si::error_code<>::create<1, boost::system::system_category>();
+	e.clear();
+	BOOST_CHECK_EQUAL(Si::error_code<>(), e);
+}
+
 BOOST_AUTO_TEST_CASE(error_code_copy)
 {
 	Si::error_code<> const e;
