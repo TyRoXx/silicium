a97bf67510b98e679e9533aaf064bdd71459cb07
TyRoXx <tyroxxxx@gmail.com>
2014-11-08 23:19:44 +0100

    deprecate deref_optional

--- silicium/observable/deref_optional.hpp
+++ silicium/observable/deref_optional.hpp
@@ -1,19 +1,22 @@
-#ifndef SILICIUM_REACTIVE_DEREF_OPTIONAL_HPP
-#define SILICIUM_REACTIVE_DEREF_OPTIONAL_HPP
+#ifndef SILICIUM_DEREF_OPTIONAL_HPP
+#define SILICIUM_DEREF_OPTIONAL_HPP
 
 #include <boost/optional.hpp>
 #include <silicium/observable/ptr_observable.hpp>
+#include <silicium/observable/while.hpp>
 #include <silicium/config.hpp>
 
 namespace Si
 {
 	template <class Input>
+	SILICIUM_DEPRECATED
 	auto deref_optional(Input &&input)
 #if !SILICIUM_COMPILER_HAS_AUTO_RETURN_TYPE
 		-> ptr_observable<typename Input::element_type, std::unique_ptr<typename Input::element_type>>
 #endif
 	{
-		typedef boost::optional<typename Input::element_type> optional_type;
+		typedef typename std::decay<Input>::type clean_input;
+		typedef boost::optional<typename clean_input::element_type> optional_type;
 		auto is_set = [](optional_type const &element) { return element.is_initialized(); };
 		auto deref = [](optional_type element) { return std::move(*element); };
 		return
