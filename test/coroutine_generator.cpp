b2466f3f56d4bedfd91aa7ea9bdc6330998d2b1c
TyRoXx <tyroxxxx@gmail.com>
2014-11-10 20:41:11 +0100

    test self-destruction on a coroutine_generator

--- test/coroutine_generator.cpp
+++ test/coroutine_generator.cpp
@@ -1,6 +1,8 @@
 #include <silicium/observable/coroutine_generator.hpp>
 #include <silicium/observable/consume.hpp>
+#include <silicium/observable/for_each.hpp>
 #include <silicium/observable/bridge.hpp>
+#include <silicium/to_unique.hpp>
 #include <boost/test/unit_test.hpp>
 
 namespace Si
@@ -59,4 +61,30 @@ namespace Si
 		std::vector<int> const expected{3};
 		BOOST_CHECK(expected == generated);
 	}
+}
+
+BOOST_AUTO_TEST_CASE(coroutine_generator_self_destruct)
+{
+	std::size_t steps_done = 0;
+	auto coro = Si::to_unique(Si::make_coroutine_generator<Si::nothing>([&steps_done](Si::push_context<Si::nothing> push) -> Si::nothing
+	{
+		BOOST_REQUIRE_EQUAL(1, steps_done);
+		++steps_done;
+		push({});
+		return {};
+	}));
+	BOOST_REQUIRE_EQUAL(0, steps_done);
+	auto handler = Si::for_each(Si::ref(*coro), [&coro, &steps_done](Si::nothing)
+	{
+		//this function is called in the coroutine
+		BOOST_REQUIRE_EQUAL(2, steps_done);
+		++steps_done;
+		//destroying the coroutine itself now should not crash or anything, it just works.
+		coro.reset();
+	});
+	BOOST_REQUIRE_EQUAL(0, steps_done);
+	++steps_done;
+	handler.start();
+	BOOST_CHECK_EQUAL(3, steps_done);
+	BOOST_CHECK(!coro);
 }
