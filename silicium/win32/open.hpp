c13932bdcd44b69b6138f90666abc6e3a9bfb1f7
TyRoXx <tyroxxxx@gmail.com>
2014-11-16 14:52:19 +0100

    implement overwrite_file for Win32

--- silicium/win32/open.hpp
+++ silicium/win32/open.hpp
@@ -2,29 +2,39 @@
 #define SILICIUM_WIN32_OPEN_HPP
 
 #include <silicium/error_or.hpp>
-#include <silicium/file_descriptor.hpp>
+#include <silicium/file_handle.hpp>
 #include <boost/filesystem/path.hpp>
 
 namespace Si
 {
-	inline error_or<file_descriptor> open_reading(boost::filesystem::path const &name)
+	inline error_or<file_handle> open_reading(boost::filesystem::path const &name)
 	{
-		native_file_handle const fd = ::CreateFileW(name.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_ALWAYS, 0, NULL);
+		native_file_descriptor const fd = ::CreateFileW(name.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_ALWAYS, 0, NULL);
 		if (fd == INVALID_HANDLE_VALUE)
 		{
 			return boost::system::error_code(GetLastError(), boost::system::system_category());
 		}
-		return file_descriptor(fd);
+		return file_handle(fd);
 	}
 
-	inline error_or<file_descriptor> create_file(boost::filesystem::path const &name)
+	inline error_or<file_handle> create_file(boost::filesystem::path const &name)
 	{
-		native_file_handle const fd = ::CreateFileW(name.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_NEW, 0, NULL);
+		native_file_descriptor const fd = ::CreateFileW(name.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_NEW, 0, NULL);
 		if (fd == INVALID_HANDLE_VALUE)
 		{
 			return boost::system::error_code(GetLastError(), boost::system::system_category());
 		}
-		return file_descriptor(fd);
+		return file_handle(fd);
+	}
+
+	inline error_or<file_handle> overwrite_file(boost::filesystem::path const &name)
+	{
+		native_file_descriptor const fd = ::CreateFileW(name.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, 0, NULL);
+		if (fd == INVALID_HANDLE_VALUE)
+		{
+			return boost::system::error_code(GetLastError(), boost::system::system_category());
+		}
+		return file_handle(fd);
 	}
 }
 
