c5c15b2a234660ed22c86110254e6df9a928a065
TyRoXx <tyroxxxx@gmail.com>
2014-11-20 21:36:10 +0100

    document the process_parameters

--- silicium/process_parameters.hpp
+++ silicium/process_parameters.hpp
@@ -12,10 +12,20 @@ namespace Si
 	struct process_parameters
 	{
 		boost::filesystem::path executable;
+
+		/// the values for the child's argv[1...]
 		std::vector<std::string> arguments;
+
+		/// must be an existing path, otherwise the child cannot launch properly
 		boost::filesystem::path current_path;
+
+		/// stdout of the child process will be written to this sink. When nullptr, output is discarded.
 		sink<char, void> *out;
+
+		/// stderr of the child process will be written to this sink. When nullptr, output is discarded.
 		sink<char, void> *err;
+
+		/// provides stdin to the child process. When nullptr, the input will be empty.
 		source<char> *in;
 
 		process_parameters();
