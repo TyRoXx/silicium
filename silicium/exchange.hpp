03691f924370ec618a5f231d3b72186fd67dda31
TyRoXx <tyroxxxx@gmail.com>
2014-11-29 20:27:20 +0100

    exchange can deal with move-only types

--- silicium/exchange.hpp
+++ silicium/exchange.hpp
@@ -5,13 +5,29 @@
 
 namespace Si
 {
+	namespace detail
+	{
+		template <class T, class U>
+		T exchange_impl(T &dest, U &&source, std::true_type)
+		{
+			auto old = std::move_if_noexcept(dest);
+			dest = std::forward<U>(source);
+			return old;
+		}
+
+		template <class T, class U>
+		T exchange_impl(T &dest, U &&source, std::false_type)
+		{
+			auto old = dest;
+			dest = std::forward<U>(source);
+			return old;
+		}
+	}
+
 	template <class T, class U>
 	T exchange(T &dest, U &&source)
 	{
-		//TODO: move
-		auto old = dest;
-		dest = std::forward<U>(source);
-		return old;
+		return detail::exchange_impl(dest, std::forward<U>(source), std::integral_constant<bool, noexcept(dest = std::forward<U>(source))>());
 	}
 }
 
