6f1fee880170f250a13b3c9c01cdb57d7b51dd77
TyRoXx <tyroxxxx@gmail.com>
2014-12-03 20:40:46 +0100

    an example for fast_variant mostly for examining the disassembly at the moment

---
+++ examples/variant.cpp
@@ -0,0 +1,21 @@
+#include <silicium/fast_variant.hpp>
+#include <ctime>
+
+int main()
+{
+	Si::fast_variant<int, std::time_t, double> r(std::time(nullptr));
+	return Si::visit<int>(
+		r,
+		[](int v)
+	{
+		return v;
+	},
+		[](std::time_t t)
+	{
+		return t != 0;
+	},
+		[](double d)
+	{
+		return std::isnan(d);
+	});
+}
