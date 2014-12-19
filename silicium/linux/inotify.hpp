7bc2112ee4a88d06ddef3b7ef13088a42d344fd4
TyRoXx <tyroxxxx@gmail.com>
2014-12-19 23:49:18 +0100

    remove inotify.cpp because I like header-only better

--- silicium/linux/inotify.hpp
+++ silicium/linux/inotify.hpp
@@ -4,10 +4,12 @@
 #include <silicium/observable/observer.hpp>
 #include <silicium/error_or.hpp>
 #include <silicium/config.hpp>
+#include <silicium/exchange.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/swap.hpp>
 #include <boost/optional.hpp>
+#include <boost/ref.hpp>
 #include <sys/inotify.h>
 #include <dirent.h>
 
@@ -74,10 +76,74 @@ namespace Si
 		{
 			typedef std::vector<file_notification> element_type;
 
-			inotify_observable();
-			explicit inotify_observable(boost::asio::io_service &io);
-			error_or<watch_descriptor> watch(boost::filesystem::path const &target, boost::uint32_t mask);
-			void async_get_one(ptr_observer<observer<element_type>> receiver);
+			inotify_observable()
+				: receiver_(nullptr)
+			{
+			}
+
+			inotify_observable(boost::asio::io_service &io)
+				: notifier(boost::in_place(boost::ref(io)))
+				, receiver_(nullptr)
+			{
+				int fd = inotify_init();
+				if (fd < 0)
+				{
+					throw boost::system::system_error(errno, boost::system::posix_category);
+				}
+				try
+				{
+					notifier->assign(fd);
+				}
+				catch (...)
+				{
+					close(fd);
+					throw;
+				}
+			}
+
+			error_or<watch_descriptor> watch(boost::filesystem::path const &target, boost::uint32_t mask)
+			{
+				assert(notifier);
+				int const wd = inotify_add_watch(notifier->native_handle(), target.string().c_str(), mask);
+				if (wd < 0)
+				{
+					return boost::system::error_code(errno, boost::system::posix_category);
+				}
+				return watch_descriptor(notifier->native_handle(), wd);
+			}
+
+			void async_get_one(ptr_observer<observer<element_type>> receiver)
+			{
+				assert(!receiver_);
+				std::size_t const min_buffer_size = sizeof(inotify_event) + NAME_MAX + 1;
+				std::size_t const additional_buffer = 8192;
+				read_buffer.resize(min_buffer_size + additional_buffer);
+				assert(notifier);
+				notifier->async_read_some(boost::asio::buffer(read_buffer), [this](boost::system::error_code error, std::size_t bytes_read)
+				{
+					if (error)
+					{
+						if (error == boost::asio::error::operation_aborted)
+						{
+							return;
+						}
+						throw std::logic_error("not implemented");
+					}
+					else
+					{
+						std::vector<file_notification> changes;
+						for (std::size_t i = 0; i < bytes_read; )
+						{
+							inotify_event const &event = *reinterpret_cast<inotify_event const *>(read_buffer.data() + i);
+							changes.emplace_back(file_notification{event.mask, boost::filesystem::path(event.name + 0, std::find(event.name + 0, event.name + event.len, '\0'))});
+							i += sizeof(inotify_event);
+							i += event.len;
+						}
+						Si::exchange(this->receiver_, nullptr)->got_element(std::move(changes));
+					}
+				});
+				receiver_ = receiver.get();
+			}
 
 		private:
 
