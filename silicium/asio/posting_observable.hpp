b4a5ddb8249dbfcb4a92bbdf974c77601e9e3f61
TyRoXx <tyroxxxx@gmail.com>
2014-12-20 23:16:43 +0100

    posting_observable keeps the io_service running with work

--- silicium/asio/posting_observable.hpp
+++ silicium/asio/posting_observable.hpp
@@ -2,22 +2,20 @@
 #define SILICIUM_ASIO_POSTING_OBSERVABLE_HPP
 
 #include <silicium/exchange.hpp>
-#include <silicium/observable/observer.hpp>
+#include <silicium/observable/function_observer.hpp>
 #include <boost/asio/io_service.hpp>
 
 namespace Si
 {
 	namespace asio
 	{
-
 		template <class Next>
-		struct posting_observable : private observer<typename Next::element_type>
+		struct posting_observable
 		{
 			typedef typename Next::element_type element_type;
 
 			explicit posting_observable(boost::asio::io_service &io, Next next)
 				: m_io(&io)
-				, m_observer(nullptr)
 				, m_next(std::move(next))
 			{
 			}
@@ -25,33 +23,28 @@ namespace Si
 			template <class Observer>
 			void async_get_one(Observer &&observer_)
 			{
-				m_observer = observer_.get();
-				m_next.async_get_one(extend(std::forward<Observer>(observer_), observe_by_ref(static_cast<observer<element_type> &>(*this))));
+				auto keep_io_running = Si::make_unique<boost::asio::io_service::work>(*m_io);
+				m_next.async_get_one(
+					make_function_observer([this, keep_io_running = std::move(keep_io_running), observer_ = std::forward<Observer>(observer_)](boost::optional<element_type> element) mutable
+				{
+					m_io->post([element = std::move(element), observer_ = std::forward<Observer>(observer_)]() mutable
+					{
+						if (element)
+						{
+							std::forward<Observer>(observer_).got_element(std::move(*element));
+						}
+						else
+						{
+							std::forward<Observer>(observer_).ended();
+						}
+					});
+				}));
 			}
 
 		private:
 
 			boost::asio::io_service *m_io;
-			observer<element_type> *m_observer;
 			Next m_next;
-
-			virtual void got_element(element_type value) SILICIUM_OVERRIDE
-			{
-				auto observer_ = Si::exchange(m_observer, nullptr);
-				m_io->post([observer_, value = std::move(value)]() mutable
-				{
-					observer_->got_element(std::move(value));
-				});
-			}
-
-			virtual void ended() SILICIUM_OVERRIDE
-			{
-				auto observer_ = Si::exchange(m_observer, nullptr);
-				m_io->post([observer_]() mutable
-				{
-					observer_->ended();
-				});
-			}
 		};
 
 		template <class Next>
