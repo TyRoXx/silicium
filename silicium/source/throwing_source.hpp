888781388e6f113b784b20db1f37828b631d694f
TyRoXx <tyroxxxx@gmail.com>
2015-02-03 20:45:09 +0100

    simplify the implementation of make_throwing_source

--- silicium/source/throwing_source.hpp
+++ silicium/source/throwing_source.hpp
@@ -1,54 +1,19 @@
 #ifndef SILICIUM_THROWING_SOURCE_HPP
 #define SILICIUM_THROWING_SOURCE_HPP
 
-#include <silicium/source/source.hpp>
+#include <silicium/source/transforming_source.hpp>
 
 namespace Si
 {
 	template <class SourceOfErrorOrs>
-	struct throwing_source
-	{
-		typedef typename SourceOfErrorOrs::element_type::value_type element_type;
-
-		throwing_source()
-		{
-		}
-
-		explicit throwing_source(SourceOfErrorOrs input)
-			: m_input(std::move(input))
-		{
-		}
-
-		iterator_range<element_type const *> map_next(std::size_t size)
-		{
-			return {};
-		}
-
-		element_type *copy_next(iterator_range<element_type *> destination)
-		{
-			auto copied = destination.begin();
-			while (copied != destination.end())
-			{
-				auto element = Si::get(m_input);
-				if (!element)
-				{
-					break;
-				}
-				*copied = std::move(element->get());
-				++copied;
-			}
-			return copied;
-		}
-
-	private:
-
-		SourceOfErrorOrs m_input;
-	};
-
-	template <class SourceOfErrorOrs>
 	auto make_throwing_source(SourceOfErrorOrs &&input)
 	{
-		return throwing_source<typename std::decay<SourceOfErrorOrs>::type>(std::forward<SourceOfErrorOrs>(input));
+		return make_transforming_source(
+			std::forward<SourceOfErrorOrs>(input),
+			[](typename std::decay<SourceOfErrorOrs>::type::element_type element)
+		{
+			return std::move(element).get();
+		});
 	}
 }
 
