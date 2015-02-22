24a4e9f34ef4cc99a59a0551a4f77bea9e89e4d9
TyRoXx <tyroxxxx@gmail.com>
2015-02-22 20:08:18 +0100

    make directory_changes header-only

--- silicium/win32/directory_changes.hpp
+++ silicium/win32/directory_changes.hpp
@@ -7,6 +7,7 @@
 #include <silicium/config.hpp>
 #include <silicium/path.hpp>
 #include <silicium/error_or.hpp>
+#include <silicium/exchange.hpp>
 #include <boost/optional.hpp>
 #include <boost/ref.hpp>
 #include <future>
@@ -76,6 +77,95 @@ namespace Si
 			BOOST_DELETED_FUNCTION(directory_changes(directory_changes const &));
 			BOOST_DELETED_FUNCTION(directory_changes &operator = (directory_changes const &));
 		};
+
+		inline directory_changes::directory_changes()
+		{
+		}
+
+		inline directory_changes::directory_changes(directory_changes &&other)
+			: is_recursive(std::move(other.is_recursive))
+			, receiver_(other.receiver_)
+			, watch_file(std::move(other.watch_file))
+			, immovable(std::move(other.immovable))
+		{
+		}
+
+		inline directory_changes::directory_changes(boost::filesystem::path const &watched, bool is_recursive)
+			: is_recursive(is_recursive)
+			, watch_file(CreateFileW(
+				watched.c_str(),
+				FILE_LIST_DIRECTORY,
+				FILE_SHARE_WRITE | FILE_SHARE_READ | FILE_SHARE_DELETE,
+				NULL,
+				OPEN_EXISTING,
+				FILE_FLAG_BACKUP_SEMANTICS,
+				NULL
+			))
+			, immovable(make_unique<immovable_state>())
+		{
+			if (watch_file.get() == INVALID_HANDLE_VALUE)
+			{
+				throw boost::system::system_error(::GetLastError(), boost::system::native_ecat);
+			}
+
+			//start running after the creation of work
+			auto &dispatcher = immovable->read_dispatcher;
+			immovable->read_runner = std::async(std::launch::async, [&dispatcher]{ dispatcher.run(); });
+		}
+
+		inline directory_changes::~directory_changes()
+		{
+			if (watch_file.get() != nullptr &&
+				watch_file.get() != INVALID_HANDLE_VALUE &&
+				!CancelIoEx(watch_file.get(), nullptr))
+			{
+				DWORD error = GetLastError();
+				if (error != ERROR_NOT_FOUND)
+				{
+					std::terminate();
+				}
+			}
+		}
+
+		inline directory_changes &directory_changes::operator = (directory_changes &&other)
+		{
+			is_recursive = other.is_recursive;
+			receiver_ = other.receiver_;
+			watch_file = std::move(other.watch_file);
+			immovable = std::move(other.immovable);
+			return *this;
+		}
+
+		inline void directory_changes::async_get_one(ptr_observer<observer<element_type>> receiver)
+		{
+			assert(!receiver_);
+			receiver_ = receiver.get();
+			assert(immovable);
+			immovable->read_dispatcher.post([this]
+			{
+				DWORD received = 0;
+				std::array<char, 0x10000> buffer;
+				DWORD const actions = FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_CREATION | FILE_NOTIFY_CHANGE_ATTRIBUTES;
+				if (!ReadDirectoryChangesW(watch_file.get(), buffer.data(), static_cast<DWORD>(buffer.size()), is_recursive, actions, &received, nullptr, nullptr))
+				{
+					//TODO: handle ERROR_NOTIFY_ENUM_DIR (overflow of event queue)
+					return;
+				}
+				std::vector<file_notification> notifications;
+				for (char *next_event = buffer.data(); ;)
+				{
+					FILE_NOTIFY_INFORMATION const &notification = reinterpret_cast<FILE_NOTIFY_INFORMATION const &>(*next_event);
+					path name(notification.FileName + 0, notification.FileName + (notification.FileNameLength / sizeof(WCHAR)));
+					notifications.emplace_back(file_notification(notification.Action, std::move(name)));
+					if (0 == notification.NextEntryOffset)
+					{
+						break;
+					}
+					next_event += notification.NextEntryOffset;
+				}
+				Si::exchange(this->receiver_, nullptr)->got_element(std::move(notifications));
+			});
+		}
 	}
 }
 
