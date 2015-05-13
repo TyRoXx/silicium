02aa535d5a0b77a0eae9a4a84919c72bdd0a9137
TyRoXx <tyroxxxx@gmail.com>
2015-05-14 00:01:30 +0200

    to_unique takes an optional template parameter for the pointee type

--- silicium/to_unique.hpp
+++ silicium/to_unique.hpp
@@ -15,6 +15,16 @@ namespace Si
 		typedef typename std::decay<T>::type decayed_T;
 		return std::unique_ptr<decayed_T>(new decayed_T(std::forward<T>(t)));
 	}
+
+	template <class Pointee, class T>
+	auto to_unique(T &&t)
+#if !SILICIUM_COMPILER_HAS_AUTO_RETURN_TYPE
+		-> std::unique_ptr<Pointee>
+#endif
+	{
+		typedef typename std::decay<T>::type decayed_T;
+		return std::unique_ptr<Pointee>(new decayed_T(std::forward<T>(t)));
+	}
 }
 
 #endif
---
+++ test/to_unique.cpp
@@ -0,0 +1,27 @@
+#include <silicium/to_unique.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(to_unique)
+{
+	std::unique_ptr<long> p = Si::to_unique(2L);
+	BOOST_REQUIRE(p);
+	BOOST_CHECK_EQUAL(2, *p);
+}
+
+struct base
+{
+	virtual ~base() {}
+};
+
+struct derived : base
+{
+};
+
+BOOST_AUTO_TEST_CASE(to_unique_explicit_pointee)
+{
+	std::unique_ptr<derived> p = Si::to_unique(derived{});
+	BOOST_REQUIRE(p);
+
+	std::unique_ptr<base> q = Si::to_unique<base>(derived{});
+	BOOST_REQUIRE(q);
+}
