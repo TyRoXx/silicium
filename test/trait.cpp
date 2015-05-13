b33ec0865e66114e0d0c9c32d0d9eb6d24f2024d
TyRoXx <tyroxxxx@gmail.com>
2015-05-14 01:14:30 +0200

    traits compile with VC++ 2013

--- test/trait.cpp
+++ test/trait.cpp
@@ -7,6 +7,15 @@
 #include <boost/preprocessor/repetition/repeat_from_to.hpp>
 #include <silicium/to_unique.hpp>
 
+#if SILICIUM_COMPILER_GENERATES_MOVES
+#	define SILICIUM_MOVABLE_MEMBER(struct_name, member_name) SILICIUM_DEFAULT_NOEXCEPT_MOVE(struct_name)
+#else
+#	define SILICIUM_MOVABLE_MEMBER(struct_name, member_name) \
+	struct_name() : member_name() BOOST_NOEXCEPT {} \
+	struct_name(struct_name &&other) BOOST_NOEXCEPT : member_name(std::move(other.member_name)) {} \
+	struct_name &operator = (struct_name &&other) BOOST_NOEXCEPT { member_name = std::move(other.member_name); return *this; }
+#endif
+
 #define SILICIUM_DETAIL_MAKE_PARAMETER(z, n, array) BOOST_PP_COMMA_IF(n) BOOST_PP_ARRAY_ELEM(n, array) BOOST_PP_CAT(arg, n)
 #define SILICIUM_DETAIL_MAKE_PARAMETERS(array) ( BOOST_PP_REPEAT(BOOST_PP_ARRAY_SIZE(array), SILICIUM_DETAIL_MAKE_PARAMETER, array) )
 
@@ -50,13 +59,15 @@
 
 #define SILICIUM_DETAIL_MAKE_ERASER(name, methods) template <class Original> struct name : interface { \
 	Original original; \
+	SILICIUM_MOVABLE_MEMBER(name, original) \
 	explicit name(Original original) : original(std::move(original)) {} \
 	BOOST_PP_SEQ_FOR_EACH(SILICIUM_DETAIL_MAKE_ERASER_METHOD, _, methods) \
 };
 
 #define SILICIUM_DETAIL_MAKE_BOX(name, methods) struct box { \
 	std::unique_ptr<interface> original; \
-	explicit name(std::unique_ptr<interface> original) : original(std::move(original)) {} \
+	SILICIUM_MOVABLE_MEMBER(name, original) \
+	explicit name(std::unique_ptr<interface> original) BOOST_NOEXCEPT : original(std::move(original)) {} \
 	BOOST_PP_SEQ_FOR_EACH(SILICIUM_DETAIL_MAKE_BOX_METHOD, _, methods) \
 };
 
