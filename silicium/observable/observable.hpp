f32c3c1b04c41d65c9d6c94a869f72f631b158b4
TyRoXx <tyroxxxx@gmail.com>
2015-05-20 19:56:42 +0200

    use the Observable trait to generate the observable interface

--- silicium/observable/observable.hpp
+++ silicium/observable/observable.hpp
@@ -7,24 +7,15 @@
 namespace Si
 {
 	template <class Element, class Observer>
-	struct observable
-	{
-		typedef Element element_type;
-
-		virtual ~observable()
-		{
-		}
-
-		virtual void async_get_one(Observer receiver) = 0;
-	};
-
-	template <class Element, class Observer>
 	SILICIUM_TRAIT_WITH_TYPEDEFS(
 		Observable,
 		typedef Element element_type;
 		,
 		((async_get_one, (1, (Observer)), void))
 	)
+
+	template <class Element, class Observer>
+	using observable = typename Observable<Element, Observer>::interface;
 }
 
 #endif
