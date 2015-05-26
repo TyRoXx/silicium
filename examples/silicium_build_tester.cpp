e037d05aad6c0d3f6256e0fbe1ac07e1dbd76b9a
TyRoXx <tyroxxxx@gmail.com>
2015-05-26 21:56:19 +0200

    begin to build another example

---
+++ examples/silicium_build_tester.cpp
@@ -0,0 +1,26 @@
+#include <silicium/async_process.hpp>
+#include <silicium/http/parse_request.hpp>
+#include <silicium/http/generate_response.hpp>
+#include <silicium/asio/tcp_acceptor.hpp>
+#include <silicium/observable/spawn_coroutine.hpp>
+#include <iostream>
+
+namespace
+{
+
+}
+
+int main()
+{
+	boost::asio::io_service io;
+	Si::spawn_coroutine([&io](Si::spawn_context yield)
+	{
+		auto acceptor = Si::asio::make_tcp_acceptor(Si::make_unique<boost::asio::ip::tcp::acceptor>(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4(), 8080)));
+		for (;;)
+		{
+			auto client = (*yield.get_one(Si::ref(acceptor)))->get();
+			assert(client);
+		}
+	});
+	io.run();
+}
