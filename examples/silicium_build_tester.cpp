6041cfae9525e06a8db0a58066b1b73e6b75278a
TyRoXx <tyroxxxx@gmail.com>
2015-06-06 21:47:21 +0200

    split functions and reduce nesting

--- examples/silicium_build_tester.cpp
+++ examples/silicium_build_tester.cpp
@@ -25,14 +25,12 @@ namespace
 		if (!!error)
 		{
 			std::cerr << "Could not respond to " << client.remote_endpoint() << ": " << error << '\n';
+			return;
 		}
-		else
+		client.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
+		if (!!error)
 		{
-			client.shutdown(boost::asio::ip::tcp::socket::shutdown_both, error);
-			if (!!error)
-			{
-				std::cerr << "Could not shutdown connection to " << client.remote_endpoint() << ": " << error << '\n';
-			}
+			std::cerr << "Could not shutdown connection to " << client.remote_endpoint() << ": " << error << '\n';
 		}
 	}
 
@@ -44,6 +42,32 @@ namespace
 			return true;
 		}
 		return false;
+	}
+
+	enum class output_chunk_result
+	{
+		more,
+		finished
+	};
+
+	output_chunk_result handle_output_chunk(Si::native_file_descriptor readable_output)
+	{
+		std::array<char, 8192> read_buffer;
+		ssize_t const read_result = read(readable_output, read_buffer.data(), read_buffer.size());
+		if (read_result < 0)
+		{
+			int errno_ = errno;
+			boost::system::error_code error(errno_, boost::system::get_system_category());
+			std::cerr << "Reading the output of the process failed with " << error << '\n';
+			return output_chunk_result::finished;
+		}
+		if (read_result == 0)
+		{
+			//end of output
+			return output_chunk_result::finished;
+		}
+		std::cerr.write(read_buffer.data(), read_result);
+		return output_chunk_result::more;
 	}
 
 	void clone(
@@ -66,23 +90,17 @@ namespace
 		input.read.close();
 		output.write.close();
 		Si::async_process &process = maybe_process.get();
-		for (;;)
+		for (bool more = true; more;)
 		{
-			std::array<char, 8192> read_buffer;
-			ssize_t const read_result = read(output.read.handle, read_buffer.data(), read_buffer.size());
-			if (read_result < 0)
+			switch (handle_output_chunk(output.read.handle))
 			{
-				int errno_ = errno;
-				boost::system::error_code error(errno_, boost::system::get_system_category());
-				std::cerr << "Reading the output of the process failed with " << error << '\n';
+			case output_chunk_result::more:
+				break;
+
+			case output_chunk_result::finished:
+				more = false;
 				break;
 			}
-			if (read_result == 0)
-			{
-				//end of output
-				break;
-			}
-			std::cerr.write(read_buffer.data(), read_result);
 		}
 		int const result = process.wait_for_exit().get();
 		if (result != 0)
@@ -107,6 +125,64 @@ namespace
 			clone(repository, repository_cache);
 			std::cerr << "Created initial clone of the repository\n";
 		}
+	}
+
+	void serve_web_client(
+		boost::asio::ip::tcp::socket &client,
+		std::string const &repository,
+		Si::absolute_path const &repository_cache,
+		Si::spawn_context &yield)
+	{
+		Si::error_or<Si::optional<Si::http::request>> const received = Si::http::receive_request(client, yield);
+		if (received.is_error())
+		{
+			std::cerr << "Error when receiving request from " << client.remote_endpoint() << ": " << received.error() << '\n';
+			return;
+		}
+
+		Si::optional<Si::http::request> const &maybe_request = received.get();
+		if (!maybe_request)
+		{
+			return respond(client, Si::make_c_str_range("400"), Si::make_c_str_range("Bad Request"), Si::make_c_str_range("the server could not parse the request"), yield);
+		}
+
+		Si::http::request const &request = *maybe_request;
+		if (request.method != "POST" && request.method != "GET")
+		{
+			return respond(client, Si::make_c_str_range("405"), Si::make_c_str_range("Method Not Allowed"), Si::make_c_str_range("this HTTP method is not supported by this server"), yield);
+		}
+
+		if (request.path != "/")
+		{
+			return respond(client, Si::make_c_str_range("404"), Si::make_c_str_range("Not Found"), Si::make_c_str_range("unknown path"), yield);
+		}
+
+		if (request.method == "POST")
+		{
+			trigger_build(repository, repository_cache);
+		}
+
+		char const * const page =
+			"<html>"
+				"<body>"
+					"<form action=\"/\" method=\"POST\">"
+						"<input type=\"submit\" value=\"Trigger build\"/>"
+					"</form>"
+				"</body>"
+			"</html>";
+		return respond(client, Si::make_c_str_range("200"), Si::make_c_str_range("OK"), Si::make_c_str_range(page), yield);
+	}
+
+	void handle_client(
+		std::shared_ptr<boost::asio::ip::tcp::socket> client,
+		std::string const &repository,
+		Si::absolute_path const &repository_cache)
+	{
+		assert(client);
+		Si::spawn_coroutine([client = std::move(client), &repository, &repository_cache](Si::spawn_context yield)
+		{
+			serve_web_client(*client, repository, repository_cache, yield);
+		});
 	}
 }
 
@@ -161,48 +237,7 @@ int main(int argc, char **argv)
 		for (;;)
 		{
 			std::shared_ptr<boost::asio::ip::tcp::socket> client = (*yield.get_one(Si::ref(acceptor))).get();
-			assert(client);
-			Si::spawn_coroutine([client = std::move(client), &repository, &absolute_cache](Si::spawn_context yield)
-			{
-				Si::error_or<Si::optional<Si::http::request>> const received = Si::http::receive_request(*client, yield);
-				if (received.is_error())
-				{
-					std::cerr << "Error when receiving request from " << client->remote_endpoint() << ": " << received.error() << '\n';
-					return;
-				}
-
-				Si::optional<Si::http::request> const &maybe_request = received.get();
-				if (!maybe_request)
-				{
-					return respond(*client, Si::make_c_str_range("400"), Si::make_c_str_range("Bad Request"), Si::make_c_str_range("the server could not parse the request"), yield);
-				}
-
-				Si::http::request const &request = *maybe_request;
-				if (request.method != "POST" && request.method != "GET")
-				{
-					return respond(*client, Si::make_c_str_range("405"), Si::make_c_str_range("Method Not Allowed"), Si::make_c_str_range("this HTTP method is not supported by this server"), yield);
-				}
-
-				if (request.path != "/")
-				{
-					return respond(*client, Si::make_c_str_range("404"), Si::make_c_str_range("Not Found"), Si::make_c_str_range("unknown path"), yield);
-				}
-
-				if (request.method == "POST")
-				{
-					trigger_build(repository, absolute_cache);
-				}
-
-				char const * const page =
-					"<html>"
-						"<body>"
-							"<form action=\"/\" method=\"POST\">"
-								"<input type=\"submit\" value=\"Trigger build\"/>"
-							"</form>"
-						"</body>"
-					"</html>";
-				return respond(*client, Si::make_c_str_range("200"), Si::make_c_str_range("OK"), Si::make_c_str_range(page), yield);
-			});
+			handle_client(std::move(client), repository, absolute_cache);
 		}
 	});
 	io.run();
