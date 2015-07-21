2eee6d8c4fd114c0ba3d68a0c2ff2d6d06c71923
TyRoXx <tyroxxxx@gmail.com>
2015-07-21 22:48:48 +0200

    loop io_service::run; handle exceptions

--- examples/websocketpp.cpp
+++ examples/websocketpp.cpp
@@ -7,44 +7,64 @@
 #include <websocketpp/server.hpp>
 #include <websocketpp/config/asio_no_tls.hpp>
 
+namespace
+{
+	void run_example_webserver()
+	{
+		typedef websocketpp::server<websocketpp::config::asio> server_type;
+
+		server_type server;
+
+		//websocketpp thinks it is OK to spam on stderr by default. I don't think so.
+		server.clear_access_channels(websocketpp::log::alevel::all);
+		server.clear_error_channels(websocketpp::log::alevel::all);
+
+		server.init_asio();
+
+		//a handler for any non-websocket HTTP requests
+		server.set_http_handler([&server](websocketpp::connection_hdl weak_connection)
+		{
+			server_type::connection_ptr const strong_connection = server.get_con_from_hdl(weak_connection);
+			assert(strong_connection);
+
+			if (strong_connection->get_request().get_method() != "GET")
+			{
+				strong_connection->set_status(websocketpp::http::status_code::method_not_allowed);
+				return;
+			}
+
+			strong_connection->set_status(websocketpp::http::status_code::ok);
+			strong_connection->set_body("<p>Hello, world!</p><p>" + strong_connection->get_request().get_header("User-Agent") + "</p>");
+			strong_connection->append_header("Content-Type", "text/html");
+		});
+
+		//a handler for websocket messages received on existing connections
+		server.set_message_handler([](websocketpp::connection_hdl connection, server_type::message_ptr message)
+		{
+
+		});
+
+		server.listen(8080);
+		server.start_accept();
+		do
+		{
+			server.run();
+		}
+		while (!server.stopped());
+	}
+}
+
 int main()
 {
-	typedef websocketpp::server<websocketpp::config::asio> server_type;
-
-	server_type server;
-
-	//websocketpp thinks it is OK to spam on stderr by default. I don't think so.
-	server.clear_access_channels(websocketpp::log::alevel::all);
-	server.clear_error_channels(websocketpp::log::alevel::all);
-
-	server.init_asio();
-
-	//a handler for any non-websocket HTTP requests
-	server.set_http_handler([&server](websocketpp::connection_hdl weak_connection)
+	try
 	{
-		server_type::connection_ptr const strong_connection = server.get_con_from_hdl(weak_connection);
-		assert(strong_connection);
-
-		if (strong_connection->get_request().get_method() != "GET")
-		{
-			strong_connection->set_status(websocketpp::http::status_code::method_not_allowed);
-			return;
-		}
-
-		strong_connection->set_status(websocketpp::http::status_code::ok);
-		strong_connection->set_body("<p>Hello, world!</p><p>" + strong_connection->get_request().get_header("User-Agent") + "</p>");
-		strong_connection->append_header("Content-Type", "text/html");
-	});
-
-	//a handler for websocket messages received on existing connections
-	server.set_message_handler([](websocketpp::connection_hdl connection, server_type::message_ptr message)
+		run_example_webserver();
+	}
+	catch (std::exception const &ex)
 	{
-
-	});
-
-	server.listen(8080);
-	server.start_accept();
-	server.run();
+		std::cerr << typeid(ex).name() << ": " << ex.what() << '\n';
+		return 1;
+	}
 }
 #else
 int main()
