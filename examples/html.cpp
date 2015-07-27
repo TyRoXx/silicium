177a4dab39cdc2d958b96579444f15334634e0d2
TyRoXx <tyroxxxx@gmail.com>
2015-07-27 14:39:23 +0200

    create HTML example with the new syntax

---
+++ examples/html.cpp
@@ -0,0 +1,29 @@
+#include <silicium/html.hpp>
+#include <silicium/sink/iterator_sink.hpp>
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	using namespace Si::html;
+	auto document =
+		tag("html",
+			tag("head",
+				tag("title",
+					text("Title")
+				)
+			)
+			+
+			tag("body",
+				text("Hello, ") + raw("<b>world</b>") + dynamic([](Si::sink<char, Si::success> &destination)
+				{
+					Si::html::unpaired_element(destination, "br");
+				})
+			)
+		);
+	std::vector<char> generated;
+	auto sink = Si::Sink<char, Si::success>::erase(Si::make_container_sink(generated));
+	document.generate(sink);
+	std::cout.write(generated.data(), generated.size());
+	std::cout << '\n';
+}
