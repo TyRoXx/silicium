0f434f108f60798f3f3ab5ad3ecbd34ec0ba62d3
TyRoXx <tyroxxxx@gmail.com>
2015-08-07 18:38:04 +0200

    avoid VC++ linker warning LNK4221 in the include tests

--- test_includes/sources/sqlite3.cpp
+++ test_includes/sources/sqlite3.cpp
@@ -1,1 +1,5 @@
 #include <silicium/sqlite3.hpp>
+namespace {
+	//"This object file does not define any previously undefined public symbols, so it will not be used by any link operation that consumes this library"
+	int dummy_to_avoid_msvc_linker_warning_LNK4221;
+}
