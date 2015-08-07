268e785c0c2daf0fca9cd345764a3356a2b4fdd0
TyRoXx <tyroxxxx@gmail.com>
2015-08-07 19:48:29 +0200

    set VC++ 2015 /await on x64 to enable experimental features

---
+++ examples/vs2015_await.cpp
@@ -0,0 +1,21 @@
+#include <experimental/filesystem>
+#include <experimental/generator>
+#include <experimental/resumable>
+#include <boost/range/algorithm/copy.hpp>
+#include <iostream>
+
+namespace
+{
+	std::experimental::generator<char> generate_something()
+	{
+		__yield_value 'x';
+	}
+}
+
+int main()
+{
+	for (auto &&e : generate_something())
+	{
+		std::cout << e;
+	}
+}
