19044a6ad5b713c71ae2112c8260ad4ed3fed93a
TyRoXx <tyroxxxx@gmail.com>
2015-08-30 23:06:32 +0200

    extract the "configure" function

--- system_test/cdm/test_using_gtest.cpp
+++ system_test/cdm/test_using_gtest.cpp
@@ -8,20 +8,16 @@ namespace
 	Si::absolute_path const test_cdm = *Si::parent(this_file);
 	Si::absolute_path const test = *Si::parent(test_cdm);
 	Si::absolute_path const silicium = *Si::parent(test);
-}
 
-BOOST_AUTO_TEST_CASE(test_using_gtest)
-{
-	Si::absolute_path const using_gtest_source = silicium / Si::relative_path("cdm/application/using_gtest");
-	Si::absolute_path const gtest_source = silicium / Si::relative_path("cdm/original_sources/gtest-1.7.0");
-	Si::absolute_path const tmp = Si::temporary_directory(Si::throw_);
-	Si::absolute_path const build_dir = tmp / *Si::path_segment::create("build");
-	Si::absolute_path const install_dir = tmp / *Si::path_segment::create("install");
-	Si::recreate_directories(build_dir, Si::throw_);
-	Si::recreate_directories(install_dir, Si::throw_);
-	cdm::gtest_paths const gtest_installed = cdm::install_gtest(gtest_source, build_dir, install_dir, Si::cmake_exe);
-	auto output = Si::Sink<char, Si::success>::erase(Si::ostream_ref_sink(std::cerr));
+	void configure(
+		Si::absolute_path const &build_dir,
+		Si::absolute_path const &install_dir,
+		Si::sink<char, Si::success> &output
+		)
 	{
+		Si::absolute_path const using_gtest_source = silicium / Si::relative_path("cdm/application/using_gtest");
+		Si::absolute_path const gtest_source = silicium / Si::relative_path("cdm/original_sources/gtest-1.7.0");
+		cdm::gtest_paths const gtest_installed = cdm::install_gtest(gtest_source, build_dir, install_dir, Si::cmake_exe);
 		std::vector<Si::os_string> arguments;
 		arguments.push_back(Si::os_string(SILICIUM_SYSTEM_LITERAL("-DGTEST_INCLUDE_DIRS=")) + to_os_string(gtest_installed.include));
 		arguments.push_back(Si::os_string(SILICIUM_SYSTEM_LITERAL("-DGTEST_LIBRARIES=")) + to_os_string(gtest_installed.library) + SILICIUM_SYSTEM_LITERAL(";") + to_os_string(gtest_installed.library_main));
@@ -29,6 +25,17 @@ BOOST_AUTO_TEST_CASE(test_using_gtest)
 		Si::recreate_directories(build_dir, Si::throw_);
 		BOOST_REQUIRE_EQUAL(0, Si::run_process(Si::cmake_exe, arguments, build_dir, output));
 	}
+}
+
+BOOST_AUTO_TEST_CASE(test_using_gtest)
+{
+	Si::absolute_path const tmp = Si::temporary_directory(Si::throw_);
+	Si::absolute_path const build_dir = tmp / *Si::path_segment::create("build");
+	Si::absolute_path const install_dir = tmp / *Si::path_segment::create("install");
+	Si::recreate_directories(build_dir, Si::throw_);
+	Si::recreate_directories(install_dir, Si::throw_);
+	auto output = Si::Sink<char, Si::success>::erase(Si::ostream_ref_sink(std::cerr));
+	configure(build_dir, install_dir, output);
 	{
 		std::vector<Si::os_string> arguments;
 		arguments.push_back(SILICIUM_SYSTEM_LITERAL("--build"));
