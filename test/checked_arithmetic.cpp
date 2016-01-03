c4e0b7dd8adf68fc36071c2df7ca65dc2202bfe2
TyRoXx <tyroxxxx@gmail.com>
2016-01-03 22:59:48 +0100

    overload checked_add for bounded_int to see how that could make sense

--- test/checked_arithmetic.cpp
+++ test/checked_arithmetic.cpp
@@ -1,4 +1,5 @@
 #include <silicium/arithmetic/add.hpp>
+#include <silicium/bounded_int.hpp>
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(checked_add_size_t)
@@ -34,4 +35,41 @@ BOOST_AUTO_TEST_CASE(operator_plus_assign_size_t)
 	Si::overflow_or<std::size_t> left = 2;
 	left += static_cast<std::size_t>(5);
 	BOOST_CHECK_EQUAL(Si::overflow_or<std::size_t>(7), left);
+}
+
+namespace Si
+{
+	template <class Int, Int Min, Int Max>
+	overflow_or<bounded_int<Int, Min, Max>> checked_add(bounded_int<Int, Min, Max> left,
+	                                                    bounded_int<Int, Min, Max> right)
+	{
+		auto sum = bounded_int<Int, Min, Max>::create(left.value() + right.value());
+		if (!sum)
+		{
+			return overflow;
+		}
+		return *sum;
+	}
+}
+
+BOOST_AUTO_TEST_CASE(checked_add_bounded_int)
+{
+	Si::overflow_or<Si::bounded_int<int, 1, 2>> a = Si::bounded_int<int, 1, 2>::literal<1>();
+	Si::overflow_or<Si::bounded_int<int, 1, 2>> b = Si::bounded_int<int, 1, 2>::literal<2>();
+	{
+		Si::overflow_or<Si::bounded_int<int, 1, 2>> c = a + a;
+		BOOST_CHECK_EQUAL((Si::bounded_int<int, 1, 2>::literal<2>()), c.value());
+	}
+	{
+		Si::overflow_or<Si::bounded_int<int, 1, 2>> d = a + b;
+		BOOST_CHECK(d.is_overflow());
+	}
+	{
+		Si::overflow_or<Si::bounded_int<int, 1, 2>> e = b + a;
+		BOOST_CHECK(e.is_overflow());
+	}
+	{
+		Si::overflow_or<Si::bounded_int<int, 1, 2>> f = b + b;
+		BOOST_CHECK(f.is_overflow());
+	}
 }
