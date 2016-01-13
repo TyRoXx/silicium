1600df0d639f99eef8ec10ac79e8bd91d389eb56
TyRoXx <tyroxxxx@gmail.com>
2016-01-13 19:49:25 +0100

    design a generalized version of std::async

--- test/asio.cpp
+++ test/asio.cpp
@@ -42,3 +42,60 @@ BOOST_AUTO_TEST_CASE(asio_make_tcp_acceptor)
 	auto b = Si::asio::make_tcp_acceptor(io, boost::asio::ip::tcp::endpoint());
 	auto c = Si::asio::make_tcp_acceptor(Si::make_unique<boost::asio::ip::tcp::acceptor>(io));
 }
+
+namespace Si
+{
+	template <class ForegroundDispatcher, class BackgroundDispatcher, class NullaryFunction, class ResultHandler>
+	auto async(ForegroundDispatcher &foreground, BackgroundDispatcher &background, NullaryFunction &&work,
+	           ResultHandler &&handle_result)
+	{
+		typedef decltype(work()) result_type;
+		typename boost::asio::handler_type<ResultHandler, void(result_type)>::type real_handler(
+		    std::forward<ResultHandler>(handle_result));
+		typename boost::asio::async_result<decltype(real_handler)> result(real_handler);
+		background.post([
+			SILICIUM_CAPTURE_EXPRESSION(work, std::forward<NullaryFunction>(work)),
+			SILICIUM_CAPTURE_EXPRESSION(real_handler, std::move(real_handler)),
+			&foreground
+		]() mutable
+		                {
+			                // unfortunately a post()ed function must be copyable currently, therefore the shared_ptr
+			                std::shared_ptr<result_type> result = Si::to_unique(work());
+			                foreground.post([
+				                SILICIUM_CAPTURE_EXPRESSION(result, std::move(result)),
+				                SILICIUM_CAPTURE_EXPRESSION(real_handler, std::move(real_handler))
+				            ]() mutable
+			                                {
+				                                real_handler(std::move(*result));
+				                            });
+			            });
+		return result.get();
+	}
+}
+
+BOOST_AUTO_TEST_CASE(asio_async)
+{
+	boost::asio::io_service background;
+	boost::asio::io_service foreground;
+	bool ok = false;
+	Si::async(foreground, background,
+	          []
+	          {
+		          return Si::to_unique(42);
+		      },
+	          [&ok](std::unique_ptr<int> result)
+	          {
+		          BOOST_REQUIRE(!ok);
+		          BOOST_REQUIRE(result);
+		          BOOST_REQUIRE_EQUAL(42, *result);
+		          ok = true;
+		      });
+	BOOST_REQUIRE(!ok);
+	foreground.run();
+	foreground.reset();
+	BOOST_REQUIRE(!ok);
+	background.run();
+	BOOST_REQUIRE(!ok);
+	foreground.run();
+	BOOST_CHECK(ok);
+}
