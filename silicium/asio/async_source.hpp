5be8ea710056a4f04b4960f2de1880dc488192bd
TyRoXx <tyroxxxx@gmail.com>
2016-01-17 00:03:32 +0100

    experimental asio::async_source adaptor

---
+++ silicium/asio/async_source.hpp
@@ -0,0 +1,56 @@
+#ifndef SILICIUM_ASIO_ASYNC_SOURCE_HPP
+#define SILICIUM_ASIO_ASYNC_SOURCE_HPP
+
+#include <silicium/asio/async.hpp>
+#include <silicium/variant.hpp>
+#include <silicium/iterator_range.hpp>
+
+namespace Si
+{
+	namespace asio
+	{
+		template <class BackgroundDispatcher, class Source>
+		struct async_source
+		{
+			typedef typename std::decay<Source>::type::element_type element_type;
+			typedef Si::variant<iterator_range<element_type const *>, element_type *> result_type;
+
+			async_source(BackgroundDispatcher background, Source source)
+			    : m_background(std::forward<BackgroundDispatcher>(background))
+			    , m_source(std::forward<Source>(source))
+			{
+			}
+
+			template <class HandlerDispatcher, class Handler>
+			auto async_get(iterator_range<element_type *> buffer, HandlerDispatcher &dispatcher,
+			               Handler &&handle_result)
+			{
+				return Si::asio::async(
+				    dispatcher, m_background,
+				    [this, buffer]() -> result_type
+				    {
+					    {
+						    iterator_range<element_type const *> const mapped =
+						        m_source.map_next((std::numeric_limits<std::size_t>::max)());
+						    if (!mapped.empty())
+						    {
+							    return mapped;
+						    }
+					    }
+					    return m_source.copy_next(buffer);
+					},
+				    [SILICIUM_CAPTURE_EXPRESSION(handle_result, std::forward<Handler>(handle_result))](
+				        result_type result) mutable
+				    {
+					    std::forward<Handler>(handle_result)(result);
+					});
+			}
+
+		private:
+			BackgroundDispatcher m_background;
+			Source m_source;
+		};
+	}
+}
+
+#endif
--- test/asio.cpp
+++ test/asio.cpp
@@ -5,6 +5,8 @@
 #include <silicium/asio/post_forwarder.hpp>
 #include <silicium/asio/tcp_acceptor.hpp>
 #include <silicium/asio/async.hpp>
+#include <silicium/asio/async_source.hpp>
+#include <silicium/source/generator_source.hpp>
 #include <algorithm>
 #include <boost/asio/io_service.hpp>
 #include <boost/test/unit_test.hpp>
@@ -64,6 +66,42 @@ BOOST_AUTO_TEST_CASE(asio_async)
 	BOOST_REQUIRE(!ok);
 	foreground.run();
 	foreground.reset();
+	BOOST_REQUIRE(!ok);
+	background.run();
+	BOOST_REQUIRE(!ok);
+	foreground.run();
+	BOOST_CHECK(ok);
+}
+
+BOOST_AUTO_TEST_CASE(asio_async_source)
+{
+	boost::asio::io_service background;
+	auto source = Si::make_generator_source([]
+	                                        {
+		                                        return 42;
+		                                    });
+	Si::asio::async_source<decltype(background) &, decltype(source) &> async(background, source);
+	std::array<int, 3> buffer;
+	boost::asio::io_service foreground;
+	bool ok = false;
+	async.async_get(Si::make_contiguous_range(buffer), foreground,
+	                [&ok, &buffer](Si::variant<Si::iterator_range<int const *>, int *> result)
+	                {
+		                BOOST_REQUIRE(!ok);
+		                Si::visit<void>(result,
+		                                [](Si::iterator_range<int const *>)
+		                                {
+			                                BOOST_FAIL("expected result to be copied to the buffer");
+			                            },
+		                                [&ok, &buffer](int *end_of_results)
+		                                {
+			                                BOOST_REQUIRE_EQUAL(buffer.data() + 3, end_of_results);
+											BOOST_REQUIRE_EQUAL(42, buffer[0]);
+											BOOST_REQUIRE_EQUAL(42, buffer[1]);
+											BOOST_REQUIRE_EQUAL(42, buffer[2]);
+											ok = true;
+			                            });
+		            });
 	BOOST_REQUIRE(!ok);
 	background.run();
 	BOOST_REQUIRE(!ok);
