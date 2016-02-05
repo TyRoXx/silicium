34e25a5346b1710666f8b9dff5b7c2935fc98b93
TyRoXx <tyroxxxx@gmail.com>
2016-02-05 22:31:19 +0100

    design an aligned_ref and a copy function with alignment support

---
+++ silicium/aligned_ref.hpp
@@ -0,0 +1,49 @@
+#ifndef SILICIUM_ALIGNED_REF_HPP
+#define SILICIUM_ALIGNED_REF_HPP
+
+#include <silicium/optional.hpp>
+#include <boost/align/is_aligned.hpp>
+#include <boost/align/assume_aligned.hpp>
+#include <algorithm>
+
+namespace Si
+{
+	template <class Element, std::size_t Alignment>
+	struct aligned_ref
+	{
+		static optional<aligned_ref> create(Element &data)
+		{
+			if (boost::alignment::is_aligned(Alignment, &data))
+			{
+				return aligned_ref(data);
+			}
+			return none;
+		}
+
+		Element &ref() const
+		{
+			return *m_data;
+		}
+
+	private:
+		Element *m_data;
+
+		explicit aligned_ref(Element &data)
+		    : m_data(&data)
+		{
+		}
+	};
+
+	template <class From, class To, std::size_t Alignment>
+	void copy(aligned_ref<From, Alignment> from, aligned_ref<To, Alignment> to, std::size_t count)
+	{
+		// raw pointers to make it as easy as possible for the optimizer to understand what is being done.
+		void const *from_ptr = &from.ref();
+		void *to_ptr = &to.ref();
+		BOOST_ALIGN_ASSUME_ALIGNED(from_ptr, Alignment);
+		BOOST_ALIGN_ASSUME_ALIGNED(to_ptr, Alignment);
+		std::copy(static_cast<From const *>(from_ptr), static_cast<From const *>(from_ptr) + count, static_cast<To *>(to_ptr));
+	}
+}
+
+#endif
---
+++ test/aligned_ref.cpp
@@ -0,0 +1,47 @@
+#include <silicium/aligned_ref.hpp>
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(aligned_ref_ok)
+{
+	std::uint32_t buffer;
+	Si::optional<Si::aligned_ref<char, sizeof(buffer)>> a =
+	    Si::aligned_ref<char, sizeof(buffer)>::create(reinterpret_cast<char &>(buffer));
+	BOOST_REQUIRE(a);
+	BOOST_CHECK_EQUAL(reinterpret_cast<char *>(&buffer), &a->ref());
+}
+
+BOOST_AUTO_TEST_CASE(aligned_ref_off_by_one)
+{
+	std::uint32_t buffer;
+	BOOST_STATIC_ASSERT(sizeof(buffer) == 4);
+	Si::optional<Si::aligned_ref<char, sizeof(buffer)>> a =
+	    Si::aligned_ref<char, sizeof(buffer)>::create(reinterpret_cast<char *>(&buffer)[1]);
+	BOOST_CHECK(!a);
+}
+
+BOOST_AUTO_TEST_CASE(aligned_ref_off_by_two)
+{
+	std::uint32_t buffer;
+	BOOST_STATIC_ASSERT(sizeof(buffer) == 4);
+	Si::optional<Si::aligned_ref<char, sizeof(buffer)>> a =
+	    Si::aligned_ref<char, sizeof(buffer)>::create(reinterpret_cast<char *>(&buffer)[2]);
+	BOOST_CHECK(!a);
+}
+
+BOOST_AUTO_TEST_CASE(aligned_ref_off_by_three)
+{
+	std::uint32_t buffer;
+	BOOST_STATIC_ASSERT(sizeof(buffer) == 4);
+	Si::optional<Si::aligned_ref<char, sizeof(buffer)>> a =
+	    Si::aligned_ref<char, sizeof(buffer)>::create(reinterpret_cast<char *>(&buffer)[3]);
+	BOOST_CHECK(!a);
+}
+
+BOOST_AUTO_TEST_CASE(aligned_ref_copy)
+{
+	std::uint32_t const from = 0x12345678;
+	std::uint32_t to = 0x00000000;
+	Si::copy(*Si::aligned_ref<char const, sizeof(from)>::create(reinterpret_cast<char const &>(from)),
+	         *Si::aligned_ref<char, sizeof(to)>::create(reinterpret_cast<char &>(to)), sizeof(from));
+	BOOST_CHECK_EQUAL(from, to);
+}
