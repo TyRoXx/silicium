bcfaf1b0e09a1abc02fa70e63a400428e80e0cc1
TyRoXx <tyroxxxx@gmail.com>
2016-02-07 03:31:57 +0100

    BOOST_NORETURN did not always exist it seems

--- silicium/terminate_on_exception.hpp
+++ silicium/terminate_on_exception.hpp
@@ -1,16 +1,16 @@
 #ifndef SILICIUM_TERMINATE_ON_EXCEPTION_HPP
 #define SILICIUM_TERMINATE_ON_EXCEPTION_HPP
 
+#include <silicium/config.hpp>
 #include <boost/throw_exception.hpp>
-#include <boost/concept_check.hpp>
 
 namespace boost
 {
 #ifdef BOOST_NO_EXCEPTIONS
-	BOOST_NORETURN
+	SILICIUM_NORETURN
 	void throw_exception(std::exception const &e)
 	{
-		boost::ignore_unused_variable_warning(e);
+		Si::ignore_unused_variable_warning(e);
 		std::terminate();
 	}
 #endif
