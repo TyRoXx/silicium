e919ad657fdcf2fc2aae81ff5276b8f8db37ff97
TyRoXx <tyroxxxx@gmail.com>
2016-09-24 20:36:57 +0200

    remove Observable-related test

--- test/asio.cpp
+++ test/asio.cpp
@@ -6,31 +6,6 @@
 #include <boost/asio/io_service.hpp>
 #include <boost/test/unit_test.hpp>
 
-#if !defined(_MSC_VER) && SILICIUM_COMPILER_HAS_AUTO_RETURN_TYPE
-
-BOOST_AUTO_TEST_CASE(asio_post)
-{
-    boost::asio::io_service io;
-    Si::bridge<int> b;
-    bool got_element = false;
-    auto forwarder = Si::asio::make_post_forwarder(
-        io, Si::ref(b), [&got_element](int element)
-        {
-            BOOST_REQUIRE(!got_element);
-            got_element = true;
-            BOOST_CHECK_EQUAL(3, element);
-        });
-    BOOST_CHECK(!got_element);
-    forwarder.start();
-    BOOST_CHECK(!got_element);
-    b.got_element(3);
-    BOOST_CHECK(!got_element);
-    io.run();
-    BOOST_CHECK(got_element);
-}
-
-#endif
-
 BOOST_AUTO_TEST_CASE(asio_async)
 {
     boost::asio::io_service background;
